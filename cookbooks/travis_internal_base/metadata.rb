811997665ae11011a3b65733d2e1aa55c13275f3
Dan Buch <daniel.buch@gmail.com>
2015-06-03 14:26:20 -0400

    Bring in the internal base VM
    in prep for packerizing the worker and jupiter brain host(s) on MacStadium

---
+++ cookbooks/travis_internal_base/metadata.rb
@@ -0,0 +1,13 @@
+name             'travis_internal_base'
+maintainer       'Travis CI GmbH'
+maintainer_email 'contact+travis-internal-base-cookbook@travis-ci.org'
+license          'MIT'
+description      'Installs/Configures travis_internal_base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apt'
+depends 'openssh'
+depends 'opsmatic'
+depends 'users'
+depends 'sudo'
