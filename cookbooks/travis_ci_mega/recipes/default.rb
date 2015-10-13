277225e5951fad210517a54700c45b89fd78c7ba
Dan Buch <daniel.buch@gmail.com>
2015-10-13 15:04:57 -0400

    Prefer `Packages.bz2` when running apt-get update

--- cookbooks/travis_ci_mega/recipes/default.rb
+++ cookbooks/travis_ci_mega/recipes/default.rb
@@ -22,7 +22,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-execute 'apt-get update -yqq'
+execute 'apt-get -o Acquire::CompressionTypes::Order::=bz2 update -yqq'
 
 package Array(node['travis_ci_mega']['prerequisite_packages']) do
   action [:install, :upgrade]
--- cookbooks/travis_ci_minimal/recipes/default.rb
+++ cookbooks/travis_ci_minimal/recipes/default.rb
@@ -22,7 +22,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-execute 'apt-get update -yqq'
+execute 'apt-get -o Acquire::CompressionTypes::Order::=bz2 update -yqq'
 
 include_recipe 'travis_build_environment'
 include_recipe 'apt'
