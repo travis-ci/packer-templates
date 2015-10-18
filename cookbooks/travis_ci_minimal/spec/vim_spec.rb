e5337b1775372d6e880b9c232292c7a9a2fcaa5c
Dan Buch <daniel.buch@gmail.com>
2015-10-18 16:42:07 -0400

    Now what is happening with vim?

--- cookbooks/travis_ci_minimal/spec/vim_spec.rb
+++ cookbooks/travis_ci_minimal/spec/vim_spec.rb
@@ -1,5 +1,7 @@
 describe 'vim installation' do
   describe command('vim --version') do
+    its(:stdout) { should_not be_empty }
+    its(:stderr) { should be_empty }
     its(:exit_status) { should eq 0 }
   end
 
@@ -10,6 +12,7 @@ describe 'vim installation' do
       end
 
       describe command('vim flower.txt -c s/blume/flower -c wq') do
+        its(:stderr) { should be_empty }
         its(:stdout) { should match 'flower' }
       end
     end
