2d6734b609da8c905d9329fad1f7b8edf82c3842
Dan Buch <daniel.buch@gmail.com>
2015-10-26 17:16:35 -0400

    Sorting `depends` bits in all metadata

--- cookbooks/travis_worker_wrapper/metadata.rb
+++ cookbooks/travis_worker_wrapper/metadata.rb
@@ -6,5 +6,5 @@ description 'Installs/Configures travis_worker_wrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-depends 'travis_internal_base'
 depends 'travis_go_worker'
+depends 'travis_internal_base'
