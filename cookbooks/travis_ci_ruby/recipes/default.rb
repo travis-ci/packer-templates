b8aa8a04121edc2206593c41d80ef201d2d335ed
Dan Buch <daniel.buch@gmail.com>
2015-10-27 13:15:13 -0400

    Ensure standard bits run before ruby bits

--- cookbooks/travis_ci_ruby/recipes/default.rb
+++ cookbooks/travis_ci_ruby/recipes/default.rb
@@ -22,6 +22,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+include_recipe 'travis_ci_standard'
 include_recipe 'rvm'
 include_recipe 'rvm::multi'
 include_recipe 'java'
