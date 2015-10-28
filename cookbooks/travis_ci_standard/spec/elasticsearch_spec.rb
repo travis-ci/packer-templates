10ac4775754cc17c61cc87e605911888844f8278
Dan Buch <daniel.buch@gmail.com>
2015-10-28 16:39:33 -0400

    Touching up the elasticsearch specs to cut down on noise

--- cookbooks/travis_ci_standard/spec/elasticsearch_spec.rb
+++ cookbooks/travis_ci_standard/spec/elasticsearch_spec.rb
@@ -1,19 +1,31 @@
 describe 'elasticsearch installation' do
+  before :all do
+    system('sudo service elasticsearch start')
+    sleep 5
+    system(
+      'curl -X GET http://localhost:9200/',
+      [:out, :err] => '/dev/null'
+    )
+    sleep 8
+    system(
+      'curl -X PUT \'http://localhost:9200/twitter/user/kimchy\' ' \
+      '-d \'{ "name" : "Shay Banon" }\'',
+      [:out, :err] => '/dev/null'
+    )
+    system(
+      'curl -X PUT \'http://localhost:9200/twitter/tweet/1\' ' \
+      '-d \' { "user": "kimchy", "postDate": "2009-11-15T13:12:00", ' \
+      '"message": "Trying out Elasticsearch" }\'',
+      [:out, :err] => '/dev/null'
+    )
+    sleep 8
+  end
+
   describe package('elasticsearch') do
     it { should be_installed }
   end
 
   describe 'elasticsearch start', sudo: true do
-    before :all do
-      system('sudo service elasticsearch start')
-      system('sleep 5')
-      system('curl -X GET http://localhost:9200/')
-      system('sleep 8')
-      system('curl -XPUT \'http://localhost:9200/twitter/user/kimchy\' -d \'{ "name" : "Shay Banon" }\'')
-      system('curl -XPUT \'http://localhost:9200/twitter/tweet/1\' -d \' { "user": "kimchy", "postDate": "2009-11-15T13:12:00", "message": "Trying out Elasticsearch" }\'')
-      system('sleep 8')
-    end
-
     describe command('curl -XGET \'http://localhost:9200/twitter/tweet/1?pretty=true\'') do
       its(:stdout) { should include('Trying out Elasticsearch') }
     end
