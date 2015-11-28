03ed5ecf0affd371fb342ef674c83810d320d695
Dan Buch <daniel.buch@gmail.com>
2015-11-27 19:43:51 -0500

    Run mysql spec commands with bash whoops

--- cookbooks/travis_ci_standard/spec/mysql_spec.rb
+++ cookbooks/travis_ci_standard/spec/mysql_spec.rb
@@ -6,20 +6,21 @@ describe 'mysql installation' do
 
   describe 'mysql commands' do
     before do
-      system('mysql <<< "DROP DATABASE travis" || true')
-      system('mysql <<< "CREATE DATABASE travis"')
-      system('mysql travis <<< "CREATE TABLE test(id int)"')
-      system("mysql travis <<< 'INSERT INTO test(id) VALUES(#{rand_id})'")
+      system('bash', '-c', 'mysql <<< "DROP DATABASE travis" || true')
+      system('bash', '-c', 'mysql <<< "CREATE DATABASE travis"')
+      system('bash', '-c', 'mysql travis <<< "CREATE TABLE test(id int)"')
+      system('bash', '-c', "mysql travis <<< 'INSERT INTO test(id) VALUES(#{rand_id})'")
     end
 
     let(:rand_id) { rand(10_000...19_999) }
 
-    describe command('mysql <<< "SHOW DATABASES"') do
+    describe command('bash', '-c', 'mysql <<< "SHOW DATABASES"') do
       its(:stdout) { should match(/^travis\$/) }
     end
 
-    describe command('mysql travis <<< "SELECT id FROM test"') do
+    describe command('bash', '-c', 'mysql travis <<< "SELECT id FROM test"') do
       its(:stdout) { should match(/^#{rand_id}$/) }
     end
   end
 end
+nd
