0ba79b3c162113cf3cdf70902dea30bdd094854e
Dan Buch <daniel.buch@gmail.com>
2015-11-30 10:37:02 -0500

    Update the mysql server specs to play more nicely

--- cookbooks/travis_ci_mega/spec/mysql_spec.rb
+++ cookbooks/travis_ci_mega/spec/mysql_spec.rb
@@ -6,20 +6,16 @@ describe 'mysql installation' do
 
   describe 'mysql commands' do
     before do
-      system('bash', '-c', 'mysql <<< "DROP DATABASE travis" || true')
-      system('bash', '-c', 'mysql <<< "CREATE DATABASE travis"')
-      system('bash', '-c', 'mysql travis <<< "CREATE TABLE test(id int)"')
-      system('bash', '-c', "mysql travis <<< 'INSERT INTO test(id) VALUES(#{rand_id})'")
+      system('mysql < ./spec/files/mysql-reset.sql')
+      system('mysql travis < ./spec/files/mysql-schema.sql')
     end
 
-    let(:rand_id) { rand(10_000...19_999) }
-
-    describe command('bash', '-c', 'mysql <<< "SHOW DATABASES"') do
-      its(:stdout) { should match(/^travis\$/) }
+    describe command('echo "SHOW DATABASES" | mysql') do
+      its(:stdout) { should match(/^travis$/) }
     end
 
-    describe command('bash', '-c', 'mysql travis <<< "SELECT id FROM test"') do
-      its(:stdout) { should match(/^#{rand_id}$/) }
+    describe command('echo "SELECT id FROM test" | mysql travis') do
+      its(:stdout) { should match(/^4$/) }
     end
   end
 end
---
+++ cookbooks/travis_ci_standard/spec/files/mysql-schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE test(id int);
+INSERT INTO test(id) VALUES(4); -- fair dice roll
