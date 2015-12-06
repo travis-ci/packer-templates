1e130e7c8aeea09639bf2d1dd913b185616c5424
Dan Buch <daniel.buch@gmail.com>
2015-12-06 08:23:16 -0500

    Run sudoers specs with sudo
    by shipping a `sudo-bash` shell wrapper and configuring rspec with it around the
    specs that need sudo access.

--- cookbooks/travis_ci_standard/spec/sudoers_spec.rb
+++ cookbooks/travis_ci_standard/spec/sudoers_spec.rb
@@ -1,18 +1,32 @@
-describe file('/etc/sudoers') do
-  it { should exist }
-  it { should be_file }
-  it { should be_mode 440 }
-  it { should be_owned_by 'root' }
-  its(:content) { should match(%r{^#includedir /etc/sudoers\.d$}) }
-end
+describe 'sudoers setup' do
+  before do
+    RSpec.configure do |c|
+      set :shell, '/tmp/sudo-bash'
+    end
+  end
 
-describe file('/etc/sudoers.d/travis') do
-  it { should exist }
-  it { should be_file }
-  it { should be_mode 440 }
-  it { should be_owned_by 'root' }
-  its(:content) { should match(/^travis ALL=\(ALL\) NOPASSWD:ALL$/) }
-  %w(authenticate env_reset mail_badpass).each do |disabled|
-    its(:content) { should match(/^Defaults !#{disabled}$/) }
+  after do
+    RSpec.configure do |c|
+      set :shell, '/bin/bash'
+    end
+  end
+
+  describe file('/etc/sudoers') do
+    it { should exist }
+    it { should be_file }
+    it { should be_mode 440 }
+    it { should be_owned_by 'root' }
+    its(:content) { should match(%r{^#includedir /etc/sudoers\.d$}) }
+  end
+
+  describe file('/etc/sudoers.d/travis') do
+    it { should exist }
+    it { should be_file }
+    it { should be_mode 440 }
+    it { should be_owned_by 'root' }
+    its(:content) { should match(/^travis ALL=\(ALL\) NOPASSWD:ALL$/) }
+    %w(authenticate env_reset mail_badpass).each do |disabled|
+      its(:content) { should match(/^Defaults !#{disabled}$/) }
+    end
   end
 end
