6bf3e57cc38c1b353a9618eb6c02b433c9cefffe
Dan Buch <daniel.buch@gmail.com>
2015-12-06 07:30:02 -0500

    Touch up the pre-chef bootstrapping
    including stronger assertions around sudoers file and configurable
    size for /tmp tmpfs mount.

---
+++ cookbooks/travis_ci_minimal/spec/sudoers_spec.rb
@@ -0,0 +1,18 @@
+describe file('/etc/sudoers') do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 440 }
+  it { should be_owned_by 'root' }
+  its(:content) { should match(%r{^#includedir /etc/sudoers\.d$}) }
+end
+
+describe file('/etc/sudoers.d/travis') do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 440 }
+  it { should be_owned_by 'root' }
+  its(:content) { should match(/^travis ALL=\(ALL\) NOPASSWD:ALL$/) }
+  %w(authenticate env_reset mail_badpass).each do |disabled|
+    its(:content) { should match(/^Defaults !#{disabled}$/) }
+  end
+end
