49504279598965780c7360170c7532c41a296eea
Igor Wiedler <igor@igor.io>
2016-04-11 18:03:22 +0200

    add source_url and issues_url to all metadata.rb files to make foodcritic happy

--- cookbooks/travis_ci_erlang/metadata.rb
+++ cookbooks/travis_ci_erlang/metadata.rb
@@ -5,6 +5,8 @@ license 'MIT'
 description 'Installs/Configures travis_ci_erlang'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
+source_url 'https://github.com/travis-ci/packer-templates'
+issues_url 'https://github.com/travis-ci/packer-templates/issues'
 
 depends 'kerl'
 depends 'kiex'
--- cookbooks/travis_internal_base/metadata.rb
+++ cookbooks/travis_internal_base/metadata.rb
@@ -5,6 +5,8 @@ license 'MIT'
 description 'Installs/Configures travis_internal_base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
+source_url 'https://github.com/travis-ci/packer-templates'
+issues_url 'https://github.com/travis-ci/packer-templates/issues'
 
 depends 'apt'
 depends 'openssh'
