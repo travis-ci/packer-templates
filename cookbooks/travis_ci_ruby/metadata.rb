a99c2aece222c178813447ea8593ce507c4f9d2e
Dan Buch <daniel.buch@gmail.com>
2016-07-04 19:51:54 -0400

    Trivial whitespace to bump ci-go, ci-minimal, and ci-ruby

--- cookbooks/travis_ci_ruby/metadata.rb
+++ cookbooks/travis_ci_ruby/metadata.rb
@@ -8,6 +8,7 @@ version '0.1.0'
 source_url 'https://github.com/travis-ci/packer-templates'
 issues_url 'https://github.com/travis-ci/packer-templates/issues'
 
+
 depends 'java'
 depends 'rvm'
 depends 'sweeper'
