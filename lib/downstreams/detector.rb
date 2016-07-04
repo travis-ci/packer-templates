629fd68f17ba6208b4d7b2199683ef4ae4d362e5
Dan Buch <daniel.buch@gmail.com>
2016-07-04 19:47:06 -0400

    Remove the "todo" comment for downstream detector

--- lib/downstreams/detector.rb
+++ lib/downstreams/detector.rb
@@ -11,14 +11,6 @@ module Downstreams
     end
 
     def detect(filenames)
-      # * walk the @packer_templates_path, get list of packer templates
-      # * for each packer template, if it contains a chef-solo provisioner,
-      #   read the members of the run_list
-      # * for each member of the run_list, evaluate/read the relevant
-      #   recipe, looking for `include_recipe`.
-      # * follow the `include_recipe` bits until (condition??)
-      # * if a git commit path is inside a found cookbook, then append
-      #   the affected template to a list (which is returned later)
       to_trigger = []
       filenames.map! { |f| File.expand_path(f) }
 
