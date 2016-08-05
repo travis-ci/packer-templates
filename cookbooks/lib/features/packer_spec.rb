f3ac1bd47a3317938c367cf75b3f581e38eb53a9
Dan Buch <dan@travis-ci.org>
2016-08-05 17:49:27 -0400

    Tweak basic suite to omit those invalid on precise

--- cookbooks/lib/features/packer_spec.rb
+++ cookbooks/lib/features/packer_spec.rb
@@ -1,4 +1,4 @@
-describe 'packer installation' do
+describe 'packer installation', precise: false do
   describe command('packer version') do
     its(:stdout) { should match(/^Packer v\d/) }
     its(:exit_status) { should eq 0 }
