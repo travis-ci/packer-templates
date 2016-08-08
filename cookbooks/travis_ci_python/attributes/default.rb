3065d9ec4a318fd3050301d59f8fea0f8e2bdbc0
Dan Buch <dan@travis-ci.org>
2016-08-07 20:02:17 -0400

    Remove travis_ci_standard dependency from travis_ci_python
    to remove the possibility of installing zero pythons instead of the desired
    nonzero pythons.

--- cookbooks/travis_ci_python/attributes/default.rb
+++ cookbooks/travis_ci_python/attributes/default.rb
@@ -1,3 +1,18 @@
+default['rvm']['default'] = '1.9.3'
+default['rvm']['rubies'] = [
+  { name: '1.9.3', arguments: '--binary --fuzzy' }
+]
+default['rvm']['gems'] = %w(nokogiri)
+
+default['travis_build_environment']['use_tmpfs_for_builds'] = false
+
+gimme_versions = %w(
+  1.4.2
+)
+
+override['gimme']['versions'] = gimme_versions
+override['gimme']['default_version'] = gimme_versions.max
+
 pythons = %w(
   2.6.9
   2.7.12
