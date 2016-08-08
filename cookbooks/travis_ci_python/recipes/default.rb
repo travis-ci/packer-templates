e3750ff2f690f3ffb0c010fad93f99252f3e9077
Dan Buch <dan@travis-ci.org>
2016-08-07 20:47:10 -0400

    Remove an extra blank line

--- cookbooks/travis_ci_python/recipes/default.rb
+++ cookbooks/travis_ci_python/recipes/default.rb
@@ -22,7 +22,6 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
 include_recipe 'apt'
 include_recipe 'travis_build_environment::apt'
 include_recipe 'travis_packer_templates'
