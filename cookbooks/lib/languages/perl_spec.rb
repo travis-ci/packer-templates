194bbd9c2bb157cc12482f4ff91af148c79e2c87
Dan Buch <dan@travis-ci.org>
2016-08-12 10:36:51 -0400

    Refine regex for perlbrew output match

--- cookbooks/lib/languages/perl_spec.rb
+++ cookbooks/lib/languages/perl_spec.rb
@@ -1,6 +1,6 @@
 describe 'perl installation', dev: true do
   describe command('perlbrew list') do
-    its(:stdout) { should match(/\* 5\.\d+ \(5\.\d+\.\d+\)/) }
+    its(:stdout) { should match(/\b5\.\d+\s+\(5\.\d+\.\d+\)/) }
   end
 
   describe command('cpanm --version') do
