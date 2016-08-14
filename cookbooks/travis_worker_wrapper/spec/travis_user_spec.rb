1b2574cd77bd31f2d02c9c0a24aa2ef5167ed612
Dan Buch <dan@travis-ci.org>
2016-08-14 08:21:37 -0400

    Assert travis user password is unexpected
    by directly fetching the shadow password whoopsie

--- cookbooks/travis_worker_wrapper/spec/travis_user_spec.rb
+++ cookbooks/travis_worker_wrapper/spec/travis_user_spec.rb
@@ -2,7 +2,10 @@ describe user('travis') do
   it { should exist }
   it { should have_home_directory('/home/travis') }
   it 'has an unexpected password' do
-    salt = subject.encrypted_password.split('$').fetch(2)
+    # get the encrypted password directly, since serverspec doesn't appear to
+    # take current uid into account, meaning it doesn't use sudo when it should
+    encrypted_password = `sudo getent shadow travis | cut -f 2 -d ':'`.strip
+    salt = encrypted_password.split('$').fetch(2)
     re_encrypted = `mkpasswd -m sha-512 travis #{salt}`.strip
     expect(subject.encrypted_password).to_not eq(re_encrypted)
   end
