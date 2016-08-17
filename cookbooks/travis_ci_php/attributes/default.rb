e59ecba98710a91e16bfce62df52ca0e8c28085d
Dan Buch <daniel.buch@gmail.com>
2016-08-17 19:24:09 -0400

    Trivial whitespace removal
    to include ci-php in this PR because it's kinda important mkay

--- cookbooks/travis_ci_php/attributes/default.rb
+++ cookbooks/travis_ci_php/attributes/default.rb
@@ -7,9 +7,7 @@ override['rvm']['gems'] = %w(
   bundler
   rake
 )
-
 override['composer']['github_oauth_token'] = '2d8490a1060eb8e8a1ae9588b14e3a039b9e01c6'
-
 override['travis_packer_templates']['job_board']['stack'] = 'php'
 override['travis_packer_templates']['job_board']['features'] = %w(
   basic
