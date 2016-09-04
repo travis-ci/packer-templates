63acc0ad77c28edd659104ad1a743ba63a5b82ac
Dan Buch <dan@travis-ci.org>
2016-09-04 16:26:45 -0400

    Try harder to start travis-worker service

--- cookbooks/travis_worker_wrapper/spec/travis_worker_spec.rb
+++ cookbooks/travis_worker_wrapper/spec/travis_worker_spec.rb
@@ -1,9 +1,20 @@
-describe service('docker'), legacy: false do
-  it { should be_enabled }
-  it { should be_running }
-end
+require 'support'
 
-describe service('travis-worker') do
-  it { should be_enabled }
-  it { should be_running }
+include Support::Helpers
+
+describe 'travis-worker setup', docker: false do
+  before :all do
+    sh('sudo service docker start')
+    sh('sudo service travis-worker start')
+  end
+
+  describe service('docker'), legacy: false do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe service('travis-worker') do
+    it { should be_enabled }
+    it { should be_running }
+  end
 end
