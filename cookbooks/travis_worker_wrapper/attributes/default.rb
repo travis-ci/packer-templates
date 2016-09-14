0555ad4f457f935a0fb3f700bac67d90a7a01c82
Dan Buch <dan@travis-ci.org>
2016-09-14 17:32:14 -0400

    Bump worker docker image to version with heartbeat support

--- cookbooks/travis_worker_wrapper/attributes/default.rb
+++ cookbooks/travis_worker_wrapper/attributes/default.rb
@@ -1,3 +1,3 @@
 default['travis_worker_wrapper']['install_type'] = 'docker'
 
-override['travis_worker']['environment']['TRAVIS_WORKER_SELF_IMAGE'] = 'quay.io/travisci/worker:v2.4.0'
+override['travis_worker']['environment']['TRAVIS_WORKER_SELF_IMAGE'] = 'quay.io/travisci/worker:v2.4.0-1-g0c969a8'
