3a07a074f5e64819d378bf08b08ec044321e0aa3
Dan Buch <dan@travis-ci.org>
2016-11-30 11:44:10 -0500

    Add more tests for mysql

--- cookbooks/lib/features/mysql_spec.rb
+++ cookbooks/lib/features/mysql_spec.rb
@@ -1,7 +1,23 @@
 describe 'mysql installation' do
+  before :all do
+    sh('sudo service mysql-5.6 start || sudo service mysql start')
+  end
+
+  after :all do
+    sh('sudo service mysql-5.6 stop || sudo service mysql stop')
+  end
+
   describe command('mysql --version') do
     its(:stdout) { should match(/^mysql /) }
     its(:exit_status) { should eq 0 }
+  end
+
+  describe file('/home/travis/.my.cnf') do
+    it { should exist }
+    it { should be_readable }
+    it { should be_readable }
+    it { should be_owned_by 'travis' }
+    it { should be_grouped_int 'travis' }
   end
 
   describe 'mysql commands' do
