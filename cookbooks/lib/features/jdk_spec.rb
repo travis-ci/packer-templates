c1ef5b2101b13968982c154b8711caed9901d2d2
Dan Buch <dan@travis-ci.org>
2016-12-02 10:58:25 -0500

    Add more JDK specs

--- cookbooks/lib/features/jdk_spec.rb
+++ cookbooks/lib/features/jdk_spec.rb
@@ -3,6 +3,19 @@ describe 'jdk installation' do
     its(:exit_status) { should eq 0 }
   end
 
+  describe file('/opt/jdk_switcher/jdk_switcher.sh'), precise: false do
+    it { should exist }
+    it { should be_readable }
+  end
+
+  describe command(%(
+    . /opt/jdk_switcher/jdk_switcher.sh;
+    jdk_switcher home default
+  )), precise: false do
+    its(:stdout) { should match(%r{/usr/lib/jvm}) }
+    its(:exit_status) { should eq 0 }
+  end
+
   it 'should have JAVA_HOME defined' do
     expect(ENV['JAVA_HOME']).to_not be_nil
     expect(ENV['JAVA_HOME']).to_not be_empty
