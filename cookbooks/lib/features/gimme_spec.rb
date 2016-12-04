7a17b5d3d388a53e5711aa281399dce5a55b8014
Dan Buch <dan@travis-ci.org>
2016-12-04 07:46:44 -0500

    See what's wrong with gimme spec

--- cookbooks/lib/features/gimme_spec.rb
+++ cookbooks/lib/features/gimme_spec.rb
@@ -5,5 +5,6 @@ describe 'gimme installation' do
 
   describe command(%(eval "$(HOME=#{Support.tmpdir} gimme 1.6.3)")) do
     its(:stdout) { should match 'go version go1.6.3' }
+    its(:stderr) { should be_empty }
   end
 end
