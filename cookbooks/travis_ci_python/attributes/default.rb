7c9edb3757fb654082b877e029427adae22a7feb
Dan Buch <dan@travis-ci.org>
2017-01-17 17:39:00 -0500

    Set correct attributes for precise python :facepalm:

--- cookbooks/travis_ci_python/attributes/default.rb
+++ cookbooks/travis_ci_python/attributes/default.rb
@@ -36,9 +36,9 @@ def python_aliases(full_name)
   [nodash[0, 3]]
 end
 
-override['travis_python']['pyenv']['pythons'] = pythons
+override['python']['pyenv']['pythons'] = pythons
 pythons.each do |full_name|
-  override['travis_python']['pyenv']['aliases'][full_name] = \
+  override['python']['pyenv']['aliases'][full_name] = \
     python_aliases(full_name)
 end
 
