d61519adb744c3c18341b4670d53d9c0c57fe1e2
Dan Buch <dan@travis-ci.org>
2017-02-09 11:54:06 -0500

    Minor updates to neo4j spec

--- cookbooks/lib/features/neo4j_spec.rb
+++ cookbooks/lib/features/neo4j_spec.rb
@@ -1,20 +1,23 @@
 describe 'neo4j installation' do
   describe command('which neo4j') do
-    its(:stdout) { should match '/local/bin/neo4j' }
+    its(:stdout) { should match 'bin/neo4j' }
   end
 
   describe 'neo4j commands', sudo: true do
+    let(:furbies) { rand(200..299) }
+
     before :all do
       sh('sudo neo4j start')
       tcpwait('127.0.0.1', 7474)
+      sh("neo4j-shell -c 'set -t int furbies #{furbies}'")
     end
 
     describe service('neo4j') do
       it { should be_running }
     end
 
-    describe command('neo4j-shell -c "set -t int height 178"; neo4j-shell -c "ls"') do
-      its(:stdout) { should include('height =', '178') }
+    describe command('neo4j-shell -c ls') do
+      its(:stdout) { should include('furbies =', furbies.to_s) }
     end
   end
 end
