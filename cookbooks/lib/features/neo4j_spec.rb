8a6e34242fc508e532c550a6dca726c02cfff999
Dan Buch <dan@travis-ci.org>
2017-02-14 11:38:20 -0500

    Correct neo4j specs to be not wrong

--- cookbooks/lib/features/neo4j_spec.rb
+++ cookbooks/lib/features/neo4j_spec.rb
@@ -9,16 +9,16 @@ describe 'neo4j installation' do
 
   describe 'neo4j commands', sudo: true do
     before :all do
-      sh('sudo neo4j start')
+      sh('sudo service neo4j start')
       tcpwait('127.0.0.1', 7474)
-      sh("neo4j-shell -v -c 'create (n:thing {furbies: #{furbies});'")
+      sh("neo4j-shell -v -c 'create (n:thing {furbies: #{furbies}});'")
     end
 
     describe service('neo4j') do
       it { should be_running }
     end
 
-    describe command("neo4j-shell -v -c 'cd 0; ls'"), precise: false do
+    describe command("neo4j-shell -v -c 'cd 0 && ls'"), precise: false do
       its(:stdout) { should include('furbies =', furbies.to_s) }
       its(:stderr) { should be_empty }
     end
