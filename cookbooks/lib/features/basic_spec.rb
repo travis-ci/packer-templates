0cdd59c70bc111c4266c9fd85d12fbbe046ec37a
Lyoness <carmen.andoh@gmail.com>
2017-05-22 12:39:16 -0400

    change travis UID to 2000

--- cookbooks/lib/features/basic_spec.rb
+++ cookbooks/lib/features/basic_spec.rb
@@ -594,7 +594,7 @@ end
 
 describe user('travis') do
   it { should exist }
-  it { should have_uid 1000 }
+  it { should have_uid 2000 }
   it { should have_home_directory '/home/travis' }
   it { should have_login_shell '/bin/bash' }
 end
