f61960ed6c7b5ad4974e147cc6b972f06527f6ab
Dan Buch <daniel.buch@gmail.com>
2017-06-28 14:44:40 -0400

    Pull more resources into tfw from the travis_worker cookbook(s?)

--- cookbooks/travis_tfw/recipes/default.rb
+++ cookbooks/travis_tfw/recipes/default.rb
@@ -24,5 +24,82 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+require 'json'
+
 include_recipe 'travis_internal_base'
 include_recipe 'travis_docker'
+
+package %w[
+  lvm2
+  xfsprogs
+]
+
+template '/usr/local/bin/travis-docker-volume-setup' do
+  source 'travis-docker-volume-setup.sh.erb'
+  owner 'root'
+  group 'root'
+  mode 0o755
+  variables(
+    device: node['travis_tfw']['docker_volume_device'],
+    metadata_size: node['travis_tfw']['docker_volume_metadata_size']
+  )
+end
+
+template '/etc/default/docker-chef' do
+  source 'etc-default-docker-chef.sh.erb'
+  owner 'root'
+  group 'root'
+  mode 0o644
+end
+
+file '/etc/default/docker' do
+  content "# this space intentionally left blank\n"
+  owner 'root'
+  group 'root'
+  mode 0o644
+end
+
+daemon_json = {
+  'graph' => node['travis_tfw']['docker_dir'],
+  'hosts' => %w[
+    tcp://127.0.0.1:4243
+    unix:///var/run/docker.sock
+  ],
+  'icc' => false,
+  'userns-remap' => 'default'
+}
+
+file '/etc/docker/daemon.json' do
+  content JSON.pretty_generate(daemon_json) + "\n"
+  owner 'root'
+  group 'root'
+  mode 0o644
+end
+
+file '/etc/docker/daemon-direct-lvm.json' do
+  content JSON.pretty_generate(
+    daemon_json.merge(
+      'storage-driver' => 'devicemapper',
+      'storage-opts' => {
+        'dm.basesize' => node['travis_tfw']['docker_dm_basesize'],
+        'dm.datadev' => '/dev/direct-lvm/data',
+        'dm.metadatadev' => '/dev/direct-lvm/metadata',
+        'dm.fs' => node['travis_tfw']['docker_dm_fs']
+      }.to_a.map { |pair| pair.join('=') }
+    )
+  ) + "\n"
+  owner 'root'
+  group 'root'
+  mode 0o644
+end
+
+template '/etc/init/docker.conf' do
+  source 'etc-init-docker.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0o644
+end
+
+service 'docker' do
+  action %i[enable start]
+end
