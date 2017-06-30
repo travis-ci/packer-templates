0bd45b45fa933dd4a0105e7b49e315dd290e06bf
Dan Buch <dan@travis-ci.org>
2017-06-30 18:02:38 -0400

    Rework the neo4j spec to account for how the service runs

--- cookbooks/lib/features/neo4j_spec.rb
+++ cookbooks/lib/features/neo4j_spec.rb
@@ -11,13 +11,9 @@ describe 'neo4j installation' do
 
   describe 'neo4j commands', sudo: true do
     before :all do
-      sh('sudo service neo4j start')
+      sh('sudo neo4j start')
       tcpwait('127.0.0.1', 7474)
       sh("neo4j-shell -v -c 'create (n:thing {furbies: #{furbies}});'")
-    end
-
-    describe service('neo4j') do
-      it { should be_running }
     end
 
     describe command("neo4j-shell -v -c 'cd 0 && ls'") do
