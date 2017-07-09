9083b4404ac22041aa69c68411112608160ef283
Dan Buch <dan@travis-ci.org>
2017-07-09 09:50:11 -0400

    Bump the docker version to a xenial variant whoopsie

--- cookbooks/travis_ci_stevonnie/attributes/default.rb
+++ cookbooks/travis_ci_stevonnie/attributes/default.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+override['travis_docker']['version'] = '17.05.0~ce-0~ubuntu-xenial'
+
 override['travis_java']['default_version'] = ''
 override['travis_java']['alternate_versions'] = []
 
