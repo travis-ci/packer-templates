c7ca9bb8b84cfa1e423d859d546ed4b5de2bf017
Lyoness <carmen.andoh@gmail.com>
2017-08-17 17:30:00 -0400

    Mercurial overrides to src as install type

--- cookbooks/travis_ci_amethyst/attributes/default.rb
+++ cookbooks/travis_ci_amethyst/attributes/default.rb
@@ -89,7 +89,7 @@ elixirs = %w[
 override['travis_build_environment']['elixir_versions'] = elixirs
 override['travis_build_environment']['default_elixir_version'] = elixirs.max
 
-override['travis_build_environment']['mercurial_install_type'] = 'pip'
+override['travis_build_environment']['mercurial_install_type'] = 'src'
 override['travis_build_environment']['mercurial_version'] = '4.2.2~trusty1'
 
 override['travis_build_environment']['update_hostname'] = false
