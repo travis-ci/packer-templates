dcbb2e326719201bc535fc8a8ae9feb4f8bbdfd5
Ed Morley <edmorley@users.noreply.github.com>
2017-08-23 13:52:03 +0100

    Update to latest PHP patch releases
    Version numbers taken from:
    https://secure.php.net/downloads.php
    
    It may also be worth adding PHP 7.1 at some point (it's actually
    listed in the cookbook defaults, but overridden by here). However
    to save space I imagine it would make sense to drop the EOL PHP 5.4
    at the same time - a decision I'll leave to another day.

--- cookbooks/travis_ci_sugilite/attributes/default.rb
+++ cookbooks/travis_ci_sugilite/attributes/default.rb
@@ -15,17 +15,17 @@ override['travis_system_info']['commands_file'] = \
 
 php_versions = %w[
   5.4.45
-  5.5.37
-  5.6.24
-  7.0.7
+  5.5.38
+  5.6.31
+  7.0.22
 ]
 override['travis_build_environment']['php_versions'] = php_versions
-override['travis_build_environment']['php_default_version'] = '5.6.24'
+override['travis_build_environment']['php_default_version'] = '5.6.31'
 override['travis_build_environment']['php_aliases'] = {
   '5.4' => '5.4.45',
-  '5.5' => '5.5.37',
-  '5.6' => '5.6.24',
-  '7.0' => '7.0.7'
+  '5.5' => '5.5.38',
+  '5.6' => '5.6.31',
+  '7.0' => '7.0.22'
 }
 
 override['travis_perlbrew']['perls'] = [
