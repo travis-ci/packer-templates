3959693ab4de004afa37e4a1baa8731fc401a5b7
Ed Morley <edmorley@users.noreply.github.com>
2017-08-24 12:48:51 +0100

    Remove stevonnie docker package version override
    Once travis-ci/travis-cookbooks#892 is merged, the default docker-ce
    package version (`17.06.1~ce-0~ubuntu`) exists for both Trusty and
    Xenial, so doesn't need to be adjusted for stevonnie.
    
    Should the Trusty and Xenial versions ever diverge again, it would be
    best to add the conditional to travis-cookbooks rather than overriding
    in packer-templates.

--- cookbooks/travis_ci_stevonnie/attributes/default.rb
+++ cookbooks/travis_ci_stevonnie/attributes/default.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-override['travis_docker']['version'] = '17.06.0~ce-0~ubuntu'
-
 override['travis_java']['default_version'] = ''
 override['travis_java']['alternate_versions'] = []
 
