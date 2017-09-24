9780c2f447f0c30bfcbb7d5b4a204e76c22b508a
Dan Buch <dan@travis-ci.org>
2017-09-24 10:57:01 -0400

    Get stuff working up until chef-solo bootstrap

--- packer-scripts/CloneTravisCookbooks.ps1
+++ packer-scripts/CloneTravisCookbooks.ps1
@@ -1,7 +1,19 @@
-mkdir c:\packertmp\chef-stuff
-cd c:\packertmp\chef-stuff
+mkdir c:/windows/temp/chef-stuff
+cd c:/windows/temp/chef-stuff
 
-git clone --depth=10 --branch $env:TRAVIS_COOKBOOKS_BRANCH $env:TRAVIS_COOKBOOKS_URL
+if ($env:TRAVIS_COOKBOOKS_BRANCH -eq '') {
+  $env:TRAVIS_COOKBOOKS_BRANCH = $env:$TRAVIS_COOKBOOKS_EDGE_BRANCH
+}
+
+if ($env:TRAVIS_COOKBOOKS_BRANCH -eq '') {
+  $env:TRAVIS_COOKBOOKS_BRANCH = 'master'
+}
+
+if ($env:TRAVIS_COOKBOOKS_URL -eq '') {
+  $env:TRAVIS_COOKBOOKS_URL = 'https://github.com/travis-ci/travis-cookbooks.git'
+}
+
+git clone --depth=10 --branch $env:TRAVIS_COOKBOOKS_BRANCH $env:TRAVIS_COOKBOOKS_URL c:/windows/temp/chef-stuff/travis-cookbooks
 
 if ($env:TRAVIS_COOKBOOKS_SHA -ne '') {
   cd travis-cookbooks
@@ -10,5 +22,5 @@ if ($env:TRAVIS_COOKBOOKS_SHA -ne '') {
 }
 
 cd travis-cookbooks
-pwd >c:\.packer-env\TRAVIS_COOKBOOKS_DIR
-git log -1 --format=%h >c:\.packer-env\TRAVIS_COOKBOOKS_SHA
+pwd >c:/.packer-env/TRAVIS_COOKBOOKS_DIR
+git log -1 --format=%h >c:/.packer-env/TRAVIS_COOKBOOKS_SHA
