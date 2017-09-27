fa7eba0168ecc2a15d55a30b049224479359fa2e
Katherine Daniels <kdaniels@travis-ci.org>
2017-09-27 18:20:26 +0200

    adding the travis user, and to the Administrators group

--- packer-scripts/PreChefBootstrap.ps1
+++ packer-scripts/PreChefBootstrap.ps1
@@ -1,5 +1,7 @@
 foreach($package in Get-Content c:/windows/temp/packages.txt) {
-  If (-Not $package -eq "") 
-    { choco install $package } 
-  Else { echo "Line $package is empty" }
+  If (-Not $package -eq "") { choco install $package }
 }
+
+echo "setting up travis user"
+New-LocalUser -Name "travis" -Description "travis user from packer" -NoPassword
+Add-LocalGroupMember -Group "Administrators" -Member "travis"
