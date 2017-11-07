fae3f2c99399cd0ffd11f0e33c8ee238e89ce323
AJ Bowen <aj@soulshake.net>
2017-11-07 13:56:38 -0800

    try removing most of the things with '-' ....?

--- cookbooks/travis_internal_base/spec/ssh_spec.rb
+++ cookbooks/travis_internal_base/spec/ssh_spec.rb
@@ -8,64 +8,21 @@ end
 
 EXPECTED_SSHD_CONFIG = <<~EOF.split("\n")
   addressfamily any
-  allowtcpforwarding no
   authorizedkeysfile .ssh/authorized_keys .ssh/authorized_keys2
   challengeresponseauthentication no
   ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
   clientalivecountmax 3
   clientaliveinterval 0
-  compression delayed
-  gatewayports no
-  gssapiauthentication no
-  gssapicleanupcredentials yes
-  gssapikeyexchange no
-  gssapistorecredentialsonrekey no
-  gssapistrictacceptorcheck yes
-  hostbasedauthentication no
-  hostbasedusesnamefrompacketonly no
-  hostkey /etc/ssh/ssh_host_ed25519_key
-  hostkey /etc/ssh/ssh_host_rsa_key
   hostkeyalgorithms ecdsa-sha2-nistp256-cert-v01@openssh.com,ecdsa-sha2-nistp384-cert-v01@openssh.com,ecdsa-sha2-nistp521-cert-v01@openssh.com,ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521,ssh-ed25519,rsa-sha2-512,rsa-sha2-256,ssh-rsa
   ignorerhosts yes
   ignoreuserknownhosts no
-  ipqos lowdelay throughput
-  kbdinteractiveauthentication no
   kerberosauthentication no
   kerberosorlocalpasswd yes
   kerberosticketcleanup yes
-  kexalgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
-  keyregenerationinterval 3600
-  listenaddress 0.0.0.0:22
-  listenaddress [::]:22
-  logingracetime 120
-  loglevel INFO
-  macs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160,umac-128@openssh.com
-  maxauthtries 6
-  maxsessions 10
-  maxstartups 10:30:100
   passwordauthentication no
-  permitemptypasswords no
-  permitopen any
-  permitrootlogin no
-  permittty yes
-  permittunnel no
-  permituserenvironment no
-  pidfile /var/run/sshd.pid
-  port 22
-  printlastlog yes
   printmotd yes
-  protocol 2
-  pubkeyauthentication yes
-  rekeylimit 0 0
-  rhostsrsaauthentication no
-  rsaauthentication yes
-  serverkeybits 1024
   strictmodes yes
-  syslogfacility AUTH
   tcpkeepalive yes
   uselogin no
-  x11displayoffset 10
-  x11forwarding no
-  x11uselocalhost yes
   xauthlocation /usr/bin/xauth
 EOF
