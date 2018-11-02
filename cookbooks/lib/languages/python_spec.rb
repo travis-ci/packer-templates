d0c1362f9474af959d7d69f852471d95bc56e118
Joep van Delft <joep@travis-ci.org>
2018-11-02 22:05:01 +0100

    Fix hardcoded python versions in spec

--- cookbooks/lib/languages/python_spec.rb
+++ cookbooks/lib/languages/python_spec.rb
@@ -39,8 +39,9 @@ describe 'python environment' do
   end
 
   {
-    'python2.7' => '2.7.14',
-    'python3.6' => '3.6.3'
+    'python2.7' => '2.7.15',
+    'python3.6' => '3.6.7',
+    'python3.7' => '3.7.1'
   }.each do |python_alias, python_version|
     describe pycommand('python -m this', version: python_alias), dev: true do
       its(:stderr) { should be_empty }
