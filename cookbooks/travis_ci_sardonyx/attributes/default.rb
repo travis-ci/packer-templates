7c39c5ca41c758f621482f8b3db23d6e9ee6abb7
Joep van Delft <joep@travis-ci.org>
2018-12-07 15:10:16 +0100

    Enable phantomjs2

--- cookbooks/travis_ci_sardonyx/attributes/default.rb
+++ cookbooks/travis_ci_sardonyx/attributes/default.rb
@@ -115,7 +115,6 @@ override['travis_postgresql']['default_version'] = '9.6'
 override['travis_postgresql']['alternate_versions'] = %w[9.4 9.5 10]
 override['travis_postgresql']['enabled'] = false # is default instance started on machine boot?
 
-# TODO: phantomjs (either make tests use phantomjs 2 or re-enable phantomjs 1)
 override['travis_packer_templates']['job_board']['features'] = %w[
   basic
   chromium
@@ -134,6 +133,7 @@ override['travis_packer_templates']['job_board']['features'] = %w[
   nodejs_interpreter
   perl_interpreter
   perlbrew
+  phantomjs
   postgresql
   python_interpreter
   redis
