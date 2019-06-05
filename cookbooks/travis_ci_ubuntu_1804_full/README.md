18b180ae459bd3c82a0abe14d39946f7fd08569d
Zhengpeng Hou <zhengpeng.hou@gmail.com>
2019-06-05 20:29:24 +0800

    Initial support to bionic with name scheme changes and two images

---
+++ cookbooks/travis_ci_ubuntu_1804_full/README.md
@@ -0,0 +1,4 @@
+travis_ci_ubuntu_1804_full Cookbook
+============================
+
+A wrapper cookbook for the ubuntu 1804 full CI image.
---
+++ cookbooks/travis_ci_ubuntu_1804_full/metadata.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+name 'travis_ci_ubuntu_1804_full'
+maintainer 'Travis CI GmbH'
+maintainer_email 'contact+travis-ci-ubuntu-1804-full-cookbook@travis-ci.org'
+license 'MIT'
+description 'Installs/Configures travis_ci_ubuntu_1804_full'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+source_url 'https://github.com/travis-ci/packer-templates'
+issues_url 'https://github.com/travis-ci/packer-templates/issues'
+
+depends 'memcached'
+depends 'openssl'
+depends 'travis_build_environment'
+depends 'travis_docker'
+depends 'travis_java'
+depends 'travis_jdk'
+depends 'travis_packer_templates'
+depends 'travis_perlbrew'
+depends 'travis_phantomjs'
+depends 'travis_postgresql'
+depends 'travis_sbt_extras'
+depends 'travis_system_info'
