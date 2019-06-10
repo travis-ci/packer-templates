c33333834eb253e76d835a878dc6ba0ccd06f382
Zhengpeng Hou <zhengpeng@travis-ci.org>
2019-06-10 16:36:59 +0800

    correct language name in 1804 minimal iamge

--- cookbooks/travis_ci_ubuntu_1804_minimal/attributes/default.rb
+++ cookbooks/travis_ci_ubuntu_1804_minimal/attributes/default.rb
@@ -62,7 +62,7 @@ override['travis_packer_templates']['job_board']['features'] = %w[
   ruby_interpreter
 ]
 override['travis_packer_templates']['job_board']['languages'] = %w[
-  __peridot__
+  __ubuntu_1804_minimal__
   minimal
   bash
   shell
