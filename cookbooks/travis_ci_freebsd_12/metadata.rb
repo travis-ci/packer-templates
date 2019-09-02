6671eb78db6d995a20c1b16073ac9288f88f3560
Damian Szymanski <damian@assembla.com>
2019-09-02 13:11:21 +0200

    Cleanup

--- cookbooks/travis_ci_freebsd_12/metadata.rb
+++ cookbooks/travis_ci_freebsd_12/metadata.rb
@@ -9,12 +9,3 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 source_url 'https://github.com/travis-ci/packer-templates'
 issues_url 'https://github.com/travis-ci/packer-templates/issues'
-
-# TODO: wat
-## depends 'travis_build_environment'
-## depends 'travis_docker'
-## depends 'travis_java'
-## depends 'travis_packer_templates'
-## depends 'travis_perlbrew'
-## depends 'travis_postgresql'
-## depends 'travis_system_info'
