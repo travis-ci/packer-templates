abb0829cfbcce8b0596277c283dee4fa09ab56fe
Zhengpeng Hou <zhengpeng@travis-ci.org>
2019-09-03 10:07:42 +0800

    formatting

--- cookbooks/lib/features/couchdb_spec.rb
+++ cookbooks/lib/features/couchdb_spec.rb
@@ -5,11 +5,6 @@ describe 'couchdb installation' do
     it { should be_installed }
   end
 
-# Disable this for now
-#  describe command('/opt/couchdb/bin/couchdb -V') do
-#    its(:exit_status) { should eq 0 }
-#  end
-
   describe 'couchdb commands', sudo: true do
     before :all do
       tries = 5
--- cookbooks/lib/features/elasticsearch_spec.rb
+++ cookbooks/lib/features/elasticsearch_spec.rb
@@ -26,19 +26,6 @@ describe 'elasticsearch installation', sudo: true do
     sleep 8
   end
 
-#  describe command(
-#    %(curl '#{db_url}/toot/1?pretty=true')
-#  ) do
-#    its(:stdout) { should include('Frying up Elastosearch') }
-#  end
-
-#  describe command(
-#    %(curl '#{db_url}/toot/_search?q=message:Frying&pretty=true')
-#  ) do
-#    its(:stdout) { should match(/"total"\s*:\s*1/) }
-#    its(:stdout) { should match(/"user"\s*:\s*"koopa93"/) }
-#    its(:stdout) { should match(/"message"\s*:\s*"Frying up Elastosearch"/) }
-#  end
   after :all do
     sh(%(curl -H "Content-Type: application/json" -X DELETE "#{db_url}"))
   end
