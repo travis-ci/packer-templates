86ef4278ac39692a470843ddbac1c157a6ffabc5
Damian Szymanski <damian@assembla.com>
2019-10-24 10:05:28 +0200

    Add simple wait for an image

--- lxd/scripts/lxc-export.sh
+++ lxd/scripts/lxc-export.sh
@@ -5,6 +5,9 @@ set -o xtrace
 
 export PATH=/snap/bin/:$PATH
 
+# wait for image
+while ! $(lxc image ls | grep -q "${IMAGE_NAME}") ; do sleep 5; echo -e . ; done
+
 lxc image export ${IMAGE_NAME} ${IMAGE_NAME} && lxc image delete "${IMAGE_NAME}"
 
 # cleanup
