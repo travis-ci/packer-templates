171b88b3dfcb42b69ab566f25e5c1eaf9f470ac5
Damian Szymanski <damian@assembla.com>
2019-10-24 09:57:35 +0200

    Update ruby install

--- lxd/scripts/ruby.sh
+++ lxd/scripts/ruby.sh
@@ -29,6 +29,7 @@ sudo apt install -yqq \
   zlib1g-dev \
   libgmp-dev \
   libreadline-dev \
+  gnupg \
   libssl-dev
 
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
@@ -59,3 +60,5 @@ EOF
 
 source "$HOME/.rvm/scripts/rvm"
 rvm install ruby-2.6.5 --autolibs=enable --fuzzy
+rvm use default
+gem i bundler
