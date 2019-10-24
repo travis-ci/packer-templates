cca96daba52b520e9b6dd71fa6ff1eb93891e47f
Damian Szymanski <damian@assembla.com>
2019-10-24 10:07:44 +0200

    Update build script

--- bin/build-lxd-image.sh
+++ bin/build-lxd-image.sh
@@ -21,16 +21,22 @@ main() {
   SSH="${BUILDER_USER}@${BUILDER_HOST}"
   rsync -aR --delete --exclude .vagrant --exclude ./lxd/.vagrant --exclude tmp ${DIR} ${SSH}:~/packer-templates/
 
-  #pushd ./lxd
-  __ssh "echo export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID > ~/.load_env"
-  __ssh "echo export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY >> ~/.load_env"
-  __ssh "echo export ARCH=$ARCH >> ~/.load_env"
-  __ssh "echo export JOB_BOARD_IMAGES_URL=$JOB_BOARD_IMAGES_URL >> ~/.load_env"
-  __ssh "echo export ARCH=$ARCH >> ~/.load_env"
+  tee .load_env <<EOF
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export ARCH=$ARCH
+export JOB_BOARD_IMAGES_URL=$JOB_BOARD_IMAGES_URL
+export ARCH=$ARCH
+EOF
+  __scp ".load_env"
   __ssh ". ~/.load_env && cd ~/packer-templates/lxd && packer build <(bin/yml2json < $1.yml)"
   __ssh "rm -f ~/.load_env"
+  rm -f .load_env
+}
 
-  #__ssh "export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID; export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY; aws s3 cp $1.tar.gz s3://travis-lxc-images/{$ARCH}/ --acl public-read"
+__scp() {
+   local file="${1}"
+   scp ${file} ${SSH}:~/
 }
 
 __ssh() {
