10d5f5e8dca9e9ea49e9d043129ef0bf66942669
Damian Szymanski <damian@assembla.com>
2019-11-18 07:41:51 +0100

    Install java from package

--- lxd/scripts/jvm.sh
+++ lxd/scripts/jvm.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 source /etc/os-release
-JAVA_11_URL="https://travis-java-archives.s3.amazonaws.com/binaries/${ID}/${VERSION_ID}/$(uname -m)/linux-openjdk11.tar.bz2"
-
 set -o errexit
-
-source /tmp/__common-lib.sh
 
 main() {
   set -o xtrace
   export DEBIAN_FRONTEND='noninteractive'
   __install_packages
-  call_build_function func_name="__java"
+  __install_java
 }
 
 __install_packages() {
@@ -21,42 +17,10 @@ __install_packages() {
 }
 
 __install_java(){
-  JAVA_URL="JAVA_${JAVA_VERSION}_URL"
-
-  if [ -z ${!JAVA_URL} ];then
-    echo "There is no JAVA in version ${JAVA_VERSION}"
-    exit 1
-  fi
-
-  mkdir -p /usr/local/lib/java${JAVA_VERSION}
-  curl -s ${!JAVA_URL} | tar xjf - -C /usr/local/lib/java${JAVA_VERSION} --strip-components 1
+  wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+  sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+  apt-get update  -yqq
+  apt-get -yqq --no-install-suggests --no-install-recommends install adoptopenjdk-${JAVA_VERSION}-hotspot
 }
-
-
-__java(){
-  __install_java
-  __setup_jvm
-}
-
-__setup_jvm(){
-  echo "if [[ -d /usr/local/lib/java${JAVA_VERSION} ]]; then
-  export JAVA_HOME=/usr/local/lib/java${JAVA_VERSION}
-  export PATH="\$JAVA_HOME/bin:\$PATH"
-fi
-" > /home/travis/.bash_profile.d/travis-java.bash
-  chmod 644 /home/travis/.bash_profile.d/travis-java.bash
-  chown travis:travis /home/travis/.bash_profile.d/travis-java.bash
-}
-
-#bionic
-__java_bionic(){
-  echo "java - no installing on bionic"
-}
-
-
-__java_xenial_ppc64le(){
-  echo "java - no installing on xenial ppc64le"
-}
-
 
 main "$@"
