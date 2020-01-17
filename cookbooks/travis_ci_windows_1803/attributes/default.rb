9483b961f2e7a8b389434701602ade5151e51b39
Cesar Souto <55247541+cesar-cs@users.noreply.github.com>
2020-01-17 12:43:07 -0300

    Update default.rb

--- cookbooks/travis_ci_windows_1803/attributes/default.rb
+++ cookbooks/travis_ci_windows_1803/attributes/default.rb
@@ -62,7 +62,7 @@ override['travis_packer_templates']['job_board']['features'] = %w[
   ruby_interpreter
 ]
 override['travis_packer_templates']['job_board']['languages'] = %w[
-  __windows_1803__
+  __windows_1809__
   bash
   minimal
   sh
