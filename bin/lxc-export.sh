d908ddbd779138eb2478a60d53af5bcfa90416ab
Damian Szymanski <damian@assembla.com>
2020-01-27 16:49:30 +0100

    Update export path

--- bin/lxc-export.sh
+++ bin/lxc-export.sh
@@ -8,4 +8,4 @@ export PATH=/snap/bin/:$PATH
 # wait for image
 while ! $(lxc image ls | grep -q "${IMAGE_NAME}") ; do sleep 5; echo -e . ; done
 
-lxc image export ${IMAGE_NAME} ${IMAGE_NAME}
+lxc image export ${IMAGE_NAME} /home/travis/${IMAGE_NAME}
