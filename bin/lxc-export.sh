26a5f42cb1ca665842694ec72c19b89b98835ce5
Damian Szymanski <damian@assembla.com>
2020-01-28 14:27:35 +0100

    Copy exported image to aws s3

--- bin/lxc-export.sh
+++ bin/lxc-export.sh
@@ -9,3 +9,14 @@ export PATH=/snap/bin/:$PATH
 while ! $(lxc image ls | grep -q "${IMAGE_NAME}") ; do sleep 5; echo -e . ; done
 
 lxc image export ${IMAGE_NAME} /home/travis/${IMAGE_NAME}
+
+mkdir -p ~/.aws
+tee ~/.aws/config <<EOF
+[default]
+EOF
+tee ~/.aws/credentials <<EOF
+[default]
+aws_access_key_id = ${LXC_AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${LXC_AWS_SECRET_ACCESS_KEY}
+EOF
+aws s3 cp /home/travis/${IMAGE_NAME}.tar.gz s3://${LXC_AWS_BUCKET}/amd64/ --profile travis
