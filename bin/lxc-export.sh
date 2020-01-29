ff9268d1b4ad646a81603acdfc73c933666febe4
Damian Szymanski <damian@assembla.com>
2020-01-29 13:26:15 +0100

    Update export script

--- bin/lxc-export.sh
+++ bin/lxc-export.sh
@@ -6,8 +6,8 @@ set -o xtrace
 export PATH=/snap/bin/:$PATH
 
 # wait for image
-while ! $(lxc image ls | grep -q "${IMAGE_NAME}") ; do sleep 5; echo -e . ; done
+while ! lxc image ls | grep -q "${IMAGE_NAME}" ; do sleep 5; echo -n . ; done
 
-lxc image export ${IMAGE_NAME} /home/travis/${IMAGE_NAME}
+lxc image export "${IMAGE_NAME}" /home/travis/"${IMAGE_NAME}"
 
-aws s3 cp /home/travis/${IMAGE_NAME}.tar.gz s3://${LXC_AWS_BUCKET}/amd64/
+aws s3 cp /home/travis/"${IMAGE_NAME}".tar.gz s3://"${LXC_AWS_BUCKET}"/amd64/
