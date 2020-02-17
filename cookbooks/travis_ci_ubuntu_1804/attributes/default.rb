5589c5d45e9fb59bf25de91f0eb6aa3e4981023c
Cesar Souto <55247541+cesar-cs@users.noreply.github.com>
2020-02-17 10:28:55 -0300

    Add openjdk 8

--- cookbooks/travis_ci_ubuntu_1804/attributes/default.rb
+++ cookbooks/travis_ci_ubuntu_1804/attributes/default.rb
@@ -66,6 +66,7 @@ if node['kernel']['machine'] == 'ppc64le'
   override['travis_java']['alternate_versions'] = %w[openjdk7]
 else
   override['travis_jdk']['versions'] = %w[
+    openjdk8
     openjdk10
     openjdk11
   ]
