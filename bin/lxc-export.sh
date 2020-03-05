1d45676821cf1ac095f8ca3ec9b25dda67cda471
Pavel Dotsulenko <pavel@travis-ci.com>
2020-03-05 18:44:50 +0200

    Update lxc-export.sh

--- bin/lxc-export.sh
+++ bin/lxc-export.sh
@@ -12,7 +12,7 @@ while ! lxc image ls | grep -q "${IMAGE_NAME}"; do
 done
 
 lxc image export "${IMAGE_NAME}" /home/travis/"${IMAGE_NAME}"
-aws s3 cp /home/travis/"${IMAGE_NAME}".tar.gz s3://"${LXC_AWS_BUCKET}"/amd64/
+aws s3 cp /home/travis/"${IMAGE_NAME}".tar.gz s3://"${LXC_AWS_BUCKET}"/amd64/ --acl public-read
 
 sha256sum /home/travis/"${IMAGE_NAME}".tar.gz | cut -f1 -d ' ' > /home/travis/"${IMAGE_NAME}"_checksum.txt
-aws s3 cp /home/travis/"${IMAGE_NAME}"_checksum.txt s3://"${LXC_AWS_BUCKET}"/amd64/
+aws s3 cp /home/travis/"${IMAGE_NAME}"_checksum.txt s3://"${LXC_AWS_BUCKET}"/amd64/ --acl public-read
