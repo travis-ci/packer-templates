dd04babf528912503c03f5942aa8b77e853feb4b
Bartlomiej <bartomiejn@gmail.com>
2020-04-19 12:33:06 +0200

    PR #770 : Offenses fail in build - testing - comment  fail block of code

--- cookbooks/travis_ci_ubuntu_2004/attributes/default.rb
+++ cookbooks/travis_ci_ubuntu_2004/attributes/default.rb
@@ -33,9 +33,7 @@ override['travis_build_environment']['system_python']['pythons'] = [] # need to 
 override['travis_build_environment']['php_versions'] = [] # need to fill in
 override['travis_build_environment']['php_aliases'] = {} # need to fill in
 
-
-# Is it required ? 
-# if node['kernel']['machine'] == "x86_64"
+# if node['kernel']['machine'] == "x86_64" # Is it required ? 
 #  arch = 'amd64'
 # else
 #  arch = node['kernel']['machine']
