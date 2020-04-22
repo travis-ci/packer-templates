7a8d8829a0ceb574d83d6b9024601dfa268af448
Bartlomiej <bartomiejn@gmail.com>
2020-04-22 16:24:34 +0200

    remove features

--- cookbooks/travis_ci_ubuntu_2004/attributes/default.rb
+++ cookbooks/travis_ci_ubuntu_2004/attributes/default.rb
@@ -157,8 +157,6 @@ override['travis_packer_templates']['job_board']['languages'] = %w[
 ]
 
 override['travis_packer_templates']['job_board']['features'] = %w[
-  generic
-  basic
   ruby_interpreter
 ]
 override['travis_packer_templates']['job_board']['languages'] = %w[
