4603e4d4b090acd3de516fa0f8a33af1e41c078c
Bartlomiej <bartomiejn@gmail.com>
2020-04-24 15:38:58 +0200

    Revert "remove features"
    This reverts commit 7a8d8829a0ceb574d83d6b9024601dfa268af448.

--- cookbooks/travis_ci_ubuntu_2004/attributes/default.rb
+++ cookbooks/travis_ci_ubuntu_2004/attributes/default.rb
@@ -159,6 +159,8 @@ override['travis_packer_templates']['job_board']['languages'] = %w[
 
 # Override values in array : minimal set of options 
 override['travis_packer_templates']['job_board']['features'] = %w[
+  generic
+  basic
   ruby_interpreter
 ]
 override['travis_packer_templates']['job_board']['languages'] = %w[
