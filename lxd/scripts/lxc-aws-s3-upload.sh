0f80423bdf36133fc40b30daac1b4c6ea203d537
Damian Szymanski <damian@assembla.com>
2020-05-22 09:44:59 +0200

    Add checksum of an image and send to s3

--- lxd/scripts/lxc-aws-s3-upload.sh
+++ lxd/scripts/lxc-aws-s3-upload.sh
@@ -5,5 +5,7 @@ set -o xtrace
 
 export PATH=/snap/bin/:$PATH
 
+sha256sum ${IMAGE_NAME}.tar.gz | awk '{print $1}' > ${IMAGE_NAME}_checksum.txt
+aws s3 cp ${IMAGE_NAME}_checksum.txt s3://travis-lxc-images/${ARCH}/ --acl public-read
 aws s3 cp ${IMAGE_NAME}.tar.gz s3://travis-lxc-images/${ARCH}/ --acl public-read
 aws s3 ls s3://travis-lxc-images/${ARCH}/${IMAGE_NAME}.tar.gz && rm -f ${IMAGE_NAME}.tar.gz
