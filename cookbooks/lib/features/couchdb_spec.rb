d15a56368bc9f69388eb10d9830689471e68f15e
Cesar Souto <cesar@travis-ci.org>
2020-05-29 14:33:39 -0300

    Conditional test for CouchDB

--- cookbooks/lib/features/couchdb_spec.rb
+++ cookbooks/lib/features/couchdb_spec.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+def couchdb_url
+  return 'http://127.0.0.1:5984' if %w[trusty xenial].include?(Support.distro)
+  return 'http://admin:travis@127.0.0.1:5984' if 'bionic'.include?(Support.distro)
+
 describe 'couchdb installation' do
   describe package('couchdb') do
     it { should be_installed }
@@ -16,20 +20,20 @@ describe 'couchdb installation' do
         retry unless tries.zero?
         raise e
       end
-      sh('curl -X PUT http://admin:travis@127.0.0.1:5984/bicycle')
-      sh('curl -X PUT http://admin:travis@127.0.0.1:5984/bicycle/bell ' \
-         '-H \'Content-Type: application/json\' -d \'{"Name":"Testname"}\'')
+      sh("curl -X PUT #{couchdb_url}/bicycle")
+      sh("curl -X PUT #{couchdb_url}/bicycle/bell " \
+         "-H \"Content-Type: application/json\" -d \"{"Name":"Testname"}\"")
     end
 
-    describe command('curl http://admin:travis@127.0.0.1:5984/') do
+    describe command("curl #{couchdb_url}/") do
       its(:stdout) { should match '"couchdb":"Welcome"' }
     end
 
-    describe command('curl -X GET http://admin:travis@127.0.0.1:5984/_all_dbs') do
+    describe command("curl -X GET #{couchdb_url}/_all_dbs") do
       its(:stdout) { should match 'bicycle' }
     end
 
-    describe command('curl -X GET http://admin:travis@127.0.0.1:5984/bicycle/bell') do
+    describe command("curl -X GET #{couchdb_url}/bicycle/bell") do
       its(:stdout) { should include('_id', 'bell', 'Name', 'Testname') }
     end
   end
