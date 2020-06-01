f32162cef9e2085ed3f97b5dd5b00a8b1e45c561
Cesar Souto <cesar@travis-ci.org>
2020-06-01 12:27:16 -0300

    Adding conditional test for Ubuntu versions, fixing

--- cookbooks/lib/languages/python_spec.rb
+++ cookbooks/lib/languages/python_spec.rb
@@ -37,25 +37,25 @@ describe 'python environment' do
     its(:stderr) { should be_empty }
     its(:stdout) { should match(/^\d+\.\d+\.\d+/) }
   end
-  
+
   if 'xenial'.include?(Support.distro)
     vers = {
-     'python2.7' => '2.7.15',
-     'python3.6' => '3.6.7',
-     'python3.7' => '3.7.1'
+      'python2.7' => '2.7.15',
+      'python3.6' => '3.6.7',
+      'python3.7' => '3.7.1'
     }
   elsif 'bionic'.include?(Support.distro)
     vers = {
-     'python2.7' => '2.7.17',
-     'python3.6' => '3.6.10',
-     'python3.7' => '3.7.6',
-     'python3.8' => '3.8.1'
+      'python2.7' => '2.7.17',
+      'python3.6' => '3.6.10',
+      'python3.7' => '3.7.6',
+      'python3.8' => '3.8.1'
     }
   elsif 'focal'.include?(Support.distro)
     vers = {
-     'python3.7' => '3.7.7',
-     'python3.8' => '3.8.3'
-    } 
+      'python3.7' => '3.7.7',
+      'python3.8' => '3.8.3'
+    }
   end
 
   vers.each do |python_alias, python_version|
