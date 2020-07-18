b9747fbd0344e5867f166ba04bad28322edbd1bc
pawelgrzeda <60094765+pawelgrzeda@users.noreply.github.com>
2020-07-18 12:12:35 +0200

    Create postgresql.rb

---
+++ cookbooks/travis_ci_ubuntu_2004/recipes/postgresql.rb
@@ -0,0 +1,26 @@
+execute 'add_postgresql_gpg_key' do
+  command 'wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -'
+end
+
+execute 'add_postgresql_repository' do
+  command 'sudo sh -c \'echo \"deb http://apt.postgresql.org/pub/repos/apt/ \'lsb_release -cs\'-pgdg main\" >> /etc/apt/sources.list.d/pgdg.list\''
+end
+
+apt_update
+
+package 'postgresql' do
+  action :install 
+end
+
+package 'postgresql-contrib' do
+  action :install
+end
+
+execute 'change_pg_hba_conf' do
+  command 'line_numb=$(grep -n \'local\' pg_hba.conf | grep \'postgres\' | grep \'trust\' | cut -d\')'
+  command 'sed -i \'\'$line_numb\'s/peer/trust/\''
+end
+
+service 'postgresql' do
+  [:enable, :restart] 
+end
