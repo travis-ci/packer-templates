c70a0b25a7a1c9433c2b29cd58f52a45839c9a74
pawelgrzeda <60094765+pawelgrzeda@users.noreply.github.com>
2020-07-19 11:40:11 +0200

    Update postgresql.rb

--- cookbooks/travis_ci_ubuntu_2004/recipes/postgresql.rb
+++ cookbooks/travis_ci_ubuntu_2004/recipes/postgresql.rb
@@ -1,26 +1,25 @@
-execute 'add_postgresql_gpg_key' do
-  command 'wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -'
+apt_repository 'postgresql' do
+  uri 'http://apt.postgresql.org/pub/repos/apt/'
+  distribution 'focal-pgdg'
+  components ['main']
+  key 'https://www.postgresql.org/media/keys/ACCC4CF8.asc'
 end
 
-execute 'add_postgresql_repository' do
-  command 'sudo sh -c \'echo \"deb http://apt.postgresql.org/pub/repos/apt/ \'lsb_release -cs\'-pgdg main\" >> /etc/apt/sources.list.d/pgdg.list\''
-end
+apt_package 'postgresql'
 
-apt_update
-
-package 'postgresql' do
-  action :install 
-end
-
-package 'postgresql-contrib' do
-  action :install
+service 'postgresql' do
+  action :stop
 end
 
 execute 'change_pg_hba_conf' do
-  command 'line_numb=$(grep -n \'local\' /etc/postgresql/12/main/pg_hba.conf | grep \'postgres\' | grep \'peer\' | cut -d\')' 
+  command 'line_numb=$(grep -n \'local\' /etc/postgresql/12/main/pg_hba.conf | grep \'postgres\' | grep \'peer\' | cut -d\')'
   command 'sed -i \'\'$line_numb\'s/peer/trust/\' /etc/postgresql/12/main/pg_hba.conf'
 end
 
 service 'postgresql' do
-  action [:enable, :restart] 
+  action :enable
+end
+
+apt_repository 'postgresql' do
+  action :remove
 end
