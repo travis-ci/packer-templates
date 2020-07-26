e8df73b8f8bfbe3a1ce218e8a2f05b1d753c53d1
pawelgrzeda <60094765+pawelgrzeda@users.noreply.github.com>
2020-07-26 12:54:26 +0200

    Update mysql.rb

--- cookbooks/travis_ci_ubuntu_2004/recipes/mysql.rb
+++ cookbooks/travis_ci_ubuntu_2004/recipes/mysql.rb
@@ -3,13 +3,5 @@ package 'mysql-server'
 package 'mysql-client'
 
 service 'mysql' do
-  [:disable, :stop]
-end
-
-service 'mysqld' do
-  [:disable, :stop]
-end
-
-service 'mysql-server' do
-  [:disable, :stop]
+  action [:disable, :stop]
 end
