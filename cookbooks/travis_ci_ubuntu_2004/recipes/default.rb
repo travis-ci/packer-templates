7efab333dc28938bff28769a8edeaf699f704d35
pawelgrzeda <60094765+pawelgrzeda@users.noreply.github.com>
2020-07-26 18:32:41 +0200

    Update default.rb

--- cookbooks/travis_ci_ubuntu_2004/recipes/default.rb
+++ cookbooks/travis_ci_ubuntu_2004/recipes/default.rb
@@ -72,7 +72,7 @@ include_recipe 'travis_phantomjs::2'
 include_recipe '::couchdb'
 include_recipe '::mongodb'
 include_recipe '::mysql'
-include_recipe '::mariadb'
+# include_recipe '::mariadb'
 include_recipe '::postgresql'
 
 # HACK: ubuntu_1804-specific shims!
