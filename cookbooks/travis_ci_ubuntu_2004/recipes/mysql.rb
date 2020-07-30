5837b29f00c6bcf31093ba6d5775f3d17a0c7760
pawelgrzeda <60094765+pawelgrzeda@users.noreply.github.com>
2020-07-30 21:02:01 +0200

    Update mysql.rb

--- cookbooks/travis_ci_ubuntu_2004/recipes/mysql.rb
+++ cookbooks/travis_ci_ubuntu_2004/recipes/mysql.rb
@@ -2,6 +2,18 @@ package 'mysql-server'
 
 package 'mysql-client'
 
+bash 'config_mysql' do
+  code <<-EOH
+    sudo mysql -e "CREATE USER 'travis'@'%' IDENTIFIED BY ''"
+    sudo mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('')"
+    sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'travis'@'%'"
+    sudo mysql -e "CREATE USER 'travis'@'localhost' IDENTIFIED BY ''"
+    sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'travis'@'localhost'"
+    sudo mysql -e "CREATE USER 'travis'@'127.0.0.1' IDENTIFIED BY ''"
+    sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'travis'@'127.0.0.1'"
+  EOH
+end
+
 service 'mysql' do
   action [:disable, :stop]
 end
