7903b4324272ecc1b3619215cbf9ed2e12ce102d
Cesar Souto <cesar@travis-ci.org>
2020-08-21 16:06:44 -0300

    Script to export image

--- bin/qemu-export.sh
+++ bin/qemu-export.sh
@@ -3,5 +3,7 @@
 set -o errexit
 set -o xtrace
 
+ls -lah /home/travis/images
+
 aws s3 cp /home/travis/images/"${IMAGE_NAME}"-qemu.img.gz s3://travis-qemu-images/amd64/ci-ubuntu-1804/ --acl public-read
 
