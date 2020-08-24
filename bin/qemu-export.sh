e3d94cc9dbe873f360fbf4166b215f646e854d94
Cesar Souto <cesar@travis-ci.org>
2020-08-23 23:26:18 -0300

    Script to export image, fix path

--- bin/qemu-export.sh
+++ bin/qemu-export.sh
@@ -5,7 +5,7 @@ set -o xtrace
 
 ls -lah /home/travis/images
 
-gzip -k "${IMAGE_NAME}"
+gzip -k /home/travis/images/"${IMAGE_NAME}"
 
 aws s3 cp /home/travis/images/"${IMAGE_NAME}".gz s3://travis-qemu-images/amd64/ci-ubuntu-1804/ --acl public-read
 
