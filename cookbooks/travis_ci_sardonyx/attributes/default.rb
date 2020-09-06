b6bc0c966b1dc585ce34c0eda14dd0163c539007
Cesar Souto <cesar@travis-ci.org>
2020-09-06 16:06:27 -0300

    Changing host config

--- cookbooks/travis_ci_sardonyx/attributes/default.rb
+++ cookbooks/travis_ci_sardonyx/attributes/default.rb
@@ -102,7 +102,7 @@ override['travis_build_environment']['otp_releases'] = []
 override['travis_build_environment']['elixir_versions'] = []
 override['travis_build_environment']['default_elixir_version'] = ''
 
-override['travis_build_environment']['update_hostname'] = false
+override['travis_build_environment']['update_hostname'] = true
 override['travis_build_environment']['update_hostname'] = true if node['kernel']['machine'] == 'ppc64le'
 override['travis_build_environment']['use_tmpfs_for_builds'] = false
 
