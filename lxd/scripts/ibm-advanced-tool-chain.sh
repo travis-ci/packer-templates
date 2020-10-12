3207d9ddb94008013920da96ed9dc7c9d28674c4
Ganesh Kumar <ganesh.k@celestialsys.com>
2020-10-12 20:43:34 +0530

    Add 14.0 version for focal lxd image ibm advanced toolchain

--- lxd/scripts/ibm-advanced-tool-chain.sh
+++ lxd/scripts/ibm-advanced-tool-chain.sh
@@ -25,14 +25,14 @@ __ibm_advaced_tool_chain_install_focal_amd64() {
 
 __ibm_advaced_tool_chain_focal_install() {
     wget --quiet -O - https://public.dhe.ibm.com/software/server/POWER/Linux/toolchain/at/ubuntu/dists/focal/6976a827.gpg.key | apt-key add -
-    echo "deb https://public.dhe.ibm.com/software/server/POWER/Linux/toolchain/at/ubuntu focal at20.04" > /etc/apt/sources.list.d/advanced_tool_chain.list
+    echo "deb https://public.dhe.ibm.com/software/server/POWER/Linux/toolchain/at/ubuntu focal at14.0" > /etc/apt/sources.list.d/advanced_tool_chain.list
 
     apt-get -y update
 
-    apt-get -y install advance-toolchain-at20.04-runtime \
-      advance-toolchain-at20.04-devel \
-      advance-toolchain-at20.04-perf \
-      advance-toolchain-at20.04-mcore-libs
+    apt-get -y install advance-toolchain-at14.0-runtime \
+      advance-toolchain-at14.0-devel \
+      advance-toolchain-at14.0-perf \
+      advance-toolchain-at14.0-mcore-libs
 }
 
 main "$@"
