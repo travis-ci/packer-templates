81a7a72b11362d952332bda360c1ea070a766871
Ganesh Kumar <ganesh.k@celestialsys.com>
2020-10-14 17:37:43 +0530

    updated typo

--- lxd/scripts/ibm-advanced-tool-chain.sh
+++ lxd/scripts/ibm-advanced-tool-chain.sh
@@ -8,31 +8,31 @@ main() {
   set -o xtrace
 
   export DEBIAN_FRONTEND='noninteractive'
-  call_build_function func_name="__ibm_advaced_tool_chain_install"
+  call_build_function func_name="__ibm_advanced_tool_chain_install"
 }
 
-__ibm_advaced_tool_chain_install() {
-    echo "IBM advaced tool chain - has no installation candidate"
+__ibm_advanced_tool_chain_install() {
+    echo "IBM advanced tool chain - has no installation candidate"
 }
 
-__ibm_advaced_tool_chain_install_focal_ppc64le() {
-    __ibm_advaced_tool_chain_focal_install
+__ibm_advanced_tool_chain_install_focal_ppc64el() {
+    __ibm_advanced_tool_chain_focal_install
 }
 
-__ibm_advaced_tool_chain_install_focal_amd64() {
-    __ibm_advaced_tool_chain_focal_install
+__ibm_advanced_tool_chain_install_focal_amd64() {
+    __ibm_advnaced_tool_chain_focal_install
 }
 
-__ibm_advaced_tool_chain_install_bionic_ppc64le() {
-    __ibm_advaced_tool_chain_bionic_install
+__ibm_advanced_tool_chain_install_bionic_ppc64el() {
+    __ibm_advanced_tool_chain_bionic_install
 }
 
-__ibm_advaced_tool_chain_install_bionic_amd64() {
-    __ibm_advaced_tool_chain_bionic_install
+__ibm_advanced_tool_chain_install_bionic_amd64() {
+    __ibm_advanced_tool_chain_bionic_install
 }
 
 
-__ibm_advaced_tool_chain_focal_install() {
+__ibm_advanced_tool_chain_focal_install() {
     wget --quiet -O - https://public.dhe.ibm.com/software/server/POWER/Linux/toolchain/at/ubuntu/dists/focal/6976a827.gpg.key | apt-key add -
     echo "deb https://public.dhe.ibm.com/software/server/POWER/Linux/toolchain/at/ubuntu focal at14.0" > /etc/apt/sources.list.d/advanced_tool_chain.list
 
@@ -44,7 +44,7 @@ __ibm_advaced_tool_chain_focal_install() {
       advance-toolchain-at14.0-mcore-libs
 }
 
-__ibm_advaced_tool_chain_bionic_install() {
+__ibm_advanced_tool_chain_bionic_install() {
     wget --quiet -O - https://public.dhe.ibm.com/software/server/POWER/Linux/toolchain/at/ubuntu/dists/bionic/6976a827.gpg.key | apt-key add -
     echo "deb https://public.dhe.ibm.com/software/server/POWER/Linux/toolchain/at/ubuntu bionic at14.0" > /etc/apt/sources.list.d/advanced_tool_chain.list
 
