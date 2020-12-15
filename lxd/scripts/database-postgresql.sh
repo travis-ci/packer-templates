34ab59207ada4a7f739a12f4689efe23a9c659ca
Cesar Souto <cesar@travis-ci.org>
2020-12-15 19:25:42 -0300

    fix database script

--- lxd/scripts/database-postgresql.sh
+++ lxd/scripts/database-postgresql.sh
@@ -8,10 +8,8 @@ main() {
 
   export DEBIAN_FRONTEND='noninteractive'
   call_build_function func_name="__install_packages"
-  __install_packages_xenial_ppc64le
-  # call_build_function func_name="__setup_pgsql"
-  __setup_pgsql_xenial_ppc64le
-  # __clear_cfg_files
+  call_build_function func_name="__setup_pgsql"
+  __clear_cfg_files
   __link_ramfs
 }
 
@@ -60,21 +58,20 @@ EOF")
 
   __turn_off_postgres_xenial_ppc64le
 
-  cat /tmp/__postgresql__9.6__main__postgresql.conf | sudo tee /etc/postgresql/9.6/main/postgresql.conf
-  cat /tmp/__postgresql__9.6__main__pg_hba.conf | sudo tee /etc/postgresql/9.6/main/pg_hba.conf
-  cat /tmp/__postgresql__9.6__initd-postgresql | sudo tee /etc/init.d/postgresql
-  
+  #cat /tmp/__postgresql__9.6__main__postgresql.conf > /etc/postgresql/9.6/main/postgresql.conf
+  #cat /tmp/__postgresql__${PGSQL_VERSION}__main__pg_hba.conf > /etc/postgresql/9.6/main/pg_hba.conf
+  #cat /tmp/__postgresql__${PGSQL_VERSION}__initd-postgresql > /etc/init.d/postgresql
 
-  chmod 755 /etc/init.d/postgresql
+  #chmod 755 /etc/init.d/postgresql
 
-  chmod 644 /etc/postgresql/9.6/main/postgresql.conf
-  chown postgres:postgres /etc/postgresql/9.6/main/postgresql.conf
+  #chmod 644 /etc/postgresql/9.6/main/postgresql.conf
+  #chown postgres:postgres /etc/postgresql/9.6/main/postgresql.conf
 
-  chmod 640 /etc/postgresql/9.6/main/pg_hba.conf
-  chown postgres:postgres /etc/postgresql/9.6/main/pg_hba.conf
+  #chmod 640 /etc/postgresql/9.6/main/pg_hba.conf
+  #chown postgres:postgres /etc/postgresql/9.6/main/pg_hba.conf
 
-  rm /lib/systemd/system/postgresql.service
-  systemctl daemon-reload
+  #rm /lib/systemd/system/postgresql.service
+  #systemctl daemon-reload
 
 }
 
