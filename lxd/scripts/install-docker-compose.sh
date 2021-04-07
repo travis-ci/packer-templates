67cf47e7b91f0277872e0a22738e7a816ba10f2c
Cesar Souto <cesar@travis-ci.org>
2021-04-07 13:07:16 -0300

    Install setuptools, cryptography

--- lxd/scripts/install-docker-compose.sh
+++ lxd/scripts/install-docker-compose.sh
@@ -5,14 +5,14 @@ source <(sudo cat  "/tmp/__common-lib.sh")
 
 main() {
   set -o xtrace
-  pip install --upgrade pip
-  python3 -m pip install --upgrade pip
+  
   export DOCKER_COMPOSE_VERSION="1.29.0"
   call_build_function func_name="__install_docker_composer"
 }
 
 __install_docker_composer() {
-
+  sudo pip3 install --upgrade pip
+  sudo pip3 install cryptography wheel setuptools
   sudo pip3 install -IU "docker-compose==${DOCKER_COMPOSE_VERSION}"
 }
 
