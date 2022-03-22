db4e6e7e58bac2a533dbd78c04505f695573c8f3
Cesar Souto <cesar@travis-ci.org>
2022-03-22 19:42:11 -0300

    Setting toolchain to ver 14.0

--- lxd/scripts/ibm-advanced-tool-chain.sh
+++ lxd/scripts/ibm-advanced-tool-chain.sh
@@ -16,15 +16,15 @@ __ibm_advanced_tool_chain_install() {
 }
 
 __ibm_advanced_tool_chain_install_focal_ppc64le() {
-    __install_ibm_tool_chain 14.0-4 'focal'
+    __install_ibm_tool_chain 14.0'focal'
 }
 
 __ibm_advanced_tool_chain_install_bionic_ppc64le() {
-    __install_ibm_tool_chain 14.0-4 'bionic'
+    __install_ibm_tool_chain 14.0 'bionic'
 }
 
 __ibm_advanced_tool_chain_install_xenial_ppc64le() {
-  __install_ibm_tool_chain 12.0-4 'xenial'
+  __install_ibm_tool_chain 12.0 'xenial'
 }
 
 __install_ibm_tool_chain() {
