3d8aaf76a21cb6e178b3db28b19a1dc569c2d038
Abdulhusain Hamid <102782989+AbdulhusainHamid1@users.noreply.github.com>
2022-05-17 20:02:23 +0530

    Update install-docker.sh to set mtu value
    Update install-docker.sh to set mtu value in /etc/docker/daemon.json.
    Setting docker mtu value. docker container mtu should be less than or equal to host mtu. It should not be more than host mtu. In case, where docker container mtu is greater than host mtu. curl downloads are failed.

--- lxd/scripts/install-docker.sh
+++ lxd/scripts/install-docker.sh
@@ -8,6 +8,11 @@ chmod +x /usr/sbin/policy-rc.d
 apt-get install -y docker.io pigz --no-install-recommends
 rm -f /usr/sbin/policy-rc.d
 mkdir -p /etc/docker
-
+if [[ $(arch) == "ppc64le" ]]
+then
+  #Update docker mtu to 1460
+  echo '{"mtu": 1460}' > /tmp/daemon.json
+  sudo cp -f /tmp/daemon.json /etc/docker/daemon.json
+fi
 # add travis to docker group
 id travis && usermod -aG docker travis
