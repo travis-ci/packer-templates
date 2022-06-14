7c94507e9625393897646c6d4cb0fb3eb23d2067
Robert Mazur <robxmazur@gmail.com>
2022-06-14 12:32:14 +0200

    Using Postgresql 14

--- cookbooks/travis_ci_ubuntu_2204/attributes/default.rb
+++ cookbooks/travis_ci_ubuntu_2204/attributes/default.rb
@@ -121,7 +121,7 @@ override['travis_build_environment']['mercurial_version'] = '5.3'
 override['travis_packer_templates']['job_board']['stack'] = 'ubuntu_2204'
 
 # not yet supported
-override['travis_postgresql']['default_version'] = '12'
+override['travis_postgresql']['default_version'] = '14'
 override['travis_postgresql']['alternate_versions'] = %w[]
 override['travis_postgresql']['enabled'] = false # is default instance started on machine boot?
 
