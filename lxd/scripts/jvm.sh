655da38b88f47b5aa6345ca93dfaa31a6c37d264
Cesar Souto <cesar@travis-ci.org>
2022-07-26 12:42:16 -0300

    Testing ant

--- lxd/scripts/jvm.sh
+++ lxd/scripts/jvm.sh
@@ -8,6 +8,7 @@ main() {
   __install_packages
   __install_java
   __install_maven
+  __install_ant
 }
 
 __install_packages() {
@@ -18,22 +19,24 @@ __install_packages() {
 }
 
 __install_java(){
-  . /etc/os-release
   wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-  echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ ${VERSION_CODENAME} main" | tee /etc/apt/sources.list.d/adoptopenjdk.list
+  sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
   rm -rf /var/lib/apt/lists/*
   apt-get update -yqq
   apt-get -yqq --no-install-suggests --no-install-recommends install adoptopenjdk-${JAVA_VERSION}-hotspot
 }
 
 __install_maven(){
-  mkdir -p /opt/mvn
-  curl https://downloads.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz|tar -xz --strip 1 -C /opt/mvn
+  apt-get -yqq --no-install-suggests --no-install-recommends install maven
+}
 
-  echo 'export MAVEN_HOME=/opt/mvn
-  export PATH=${MAVEN_HOME}/bin:${PATH}' > /home/travis/.bash_profile.d/mvn.bash
-  chmod 644 /home/travis/.bash_profile.d/mvn.bash
-  chown travis: /home/travis/.bash_profile.d/mvn.bash
+__install_ant() {
+  mkdir -p /opt/ant
+  curl -sL https://downloads.apache.org//ant/binaries/apache-ant-1.10.11-bin.tar.gz | tar -xz --strip 1 -C /opt/ant
+  echo 'export ANT_HOME=/opt/ant
+  export PATH=${ANT_HOME}/bin:${PATH}' > /home/travis/.bash_profile.d/ant.bash
+  chmod 644 /home/travis/.bash_profile.d/ant.bash
+  chown travis: /home/travis/.bash_profile.d/ant.bash
 }
 
 main "$@"
