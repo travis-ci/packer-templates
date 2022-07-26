7facc8d8cb799fcc5a91ab838579386c6387f3a4
Cesar Souto <cesar@travis-ci.org>
2022-07-26 15:51:40 -0300

    Fixing network and python

--- lxd/scripts/python.sh
+++ lxd/scripts/python.sh
@@ -85,6 +85,8 @@ __install_default_python() {
 
   PYTHON_CONFIGURE_OPTS="--enable-unicode=ucs4 --with-wide-unicode --enable-shared --enable-ipv6 --enable-loadable-sqlite-extensions --with-computed-gotos"
   PYTHON_CFLAGS="-I/usr/include/openssl -g -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security"
+  
+  CFLAGS=-I/usr/include/openssl
   LDFLAGS="-L/usr/lib"
   
   pyenv install 3.10.5
