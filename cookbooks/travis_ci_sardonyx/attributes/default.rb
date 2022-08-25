a505561f8f997ac8b996a5012831191357dbc2f1
Robert Mazur <robxmazur@gmail.com>
2022-08-24 21:13:04 -0300

    Removing Python sorting

--- cookbooks/travis_ci_sardonyx/attributes/default.rb
+++ cookbooks/travis_ci_sardonyx/attributes/default.rb
@@ -62,31 +62,30 @@ override['travis_build_environment']['nodejs_versions'] = %w[
 override['travis_build_environment']['nodejs_default'] = '16.16.0'
 
 pythons = %w[
-  2.7.15
   3.7.13
   3.8.13
 ]
 
 # Reorder pythons so that default python2 and python3 come first
 # as this affects the ordering in $PATH.
-%w[3 2].each do |pyver|
-  pythons.select { |p| p =~ /^#{pyver}/ }.max.tap do |py|
-    pythons.unshift(pythons.delete(py))
-  end
-end
+# %w[3 2].each do |pyver|
+#   pythons.select { |p| p =~ /^#{pyver}/ }.max.tap do |py|
+#     pythons.unshift(pythons.delete(py))
+#   end
+# end
 
-def python_aliases(full_name)
-  nodash = full_name.split('-').first
-  return [nodash] unless nodash.include?('.')
+# def python_aliases(full_name)
+#   nodash = full_name.split('-').first
+#   return [nodash] unless nodash.include?('.')
 
-  [nodash[0, 3]]
-end
+#   [nodash[0, 3]]
+# end
 
 override['travis_build_environment']['pythons'] = pythons
-pythons.each do |full_name|
-  override['travis_build_environment']['python_aliases'][full_name] = \
-    python_aliases(full_name)
-end
+# pythons.each do |full_name|
+#   override['travis_build_environment']['python_aliases'][full_name] = \
+#     python_aliases(full_name)
+# end
 
 rubies = %w[
   2.7.6
