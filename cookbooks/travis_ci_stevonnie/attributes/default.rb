812b554207a6ab7b343011fd124ffe8a9860517d
Robert Mazur <robxmazur@gmail.com>
2022-09-06 14:22:22 -0300

    Fixing Ruby

--- cookbooks/travis_ci_stevonnie/attributes/default.rb
+++ cookbooks/travis_ci_stevonnie/attributes/default.rb
@@ -66,7 +66,7 @@ rubies = %w[
   3.1.2
 ]
 
-override['travis_build_environment']['default_ruby'] = 2.7.6
+override['travis_build_environment']['default_ruby'] = %w(2.7.6)
 override['travis_build_environment']['rubies'] = rubies
 override['travis_build_environment']['php_versions'] = []
 override['travis_build_environment']['php_aliases'] = {}
