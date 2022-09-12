59050d26eb5b74802bcc2ad0aa57ce173739e533
Robert Mazur <robxmazur@gmail.com>
2022-09-12 16:42:32 -0300

    Fix spec for installed python

--- cookbooks/lib/features/pyenv_spec.rb
+++ cookbooks/lib/features/pyenv_spec.rb
@@ -4,7 +4,7 @@ require 'json'
 
 describe 'pyenv', dev: true do
   describe command('pyenv version-name') do
-    its(:stdout) { should eql("system\n") }
+    its(:stdout) { should eql("3.7.13\n") }
   end
 
   describe command('pyenv root') do
