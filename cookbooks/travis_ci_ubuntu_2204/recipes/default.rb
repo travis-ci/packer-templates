def2df685106f36f2bd9666a105fe14755605c4c
Konrad Wrzos <konrad@travis-ci.org>
2023-02-02 14:19:20 +0100

    Adding git repository

--- cookbooks/travis_ci_ubuntu_2204/recipes/default.rb
+++ cookbooks/travis_ci_ubuntu_2204/recipes/default.rb
@@ -62,6 +62,7 @@ include_recipe 'memcached'
 # https://github.com/travis-ci/packer-templates/issues/589
 # include_recipe 'travis_build_environment::cassandra'
 # include_recipe 'travis_build_environment::couchdb'
+include_recipe 'travis_build_environment::git'
 include_recipe 'travis_build_environment::elasticsearch'
 include_recipe 'travis_build_environment::xserver'
 include_recipe 'travis_build_environment::google_chrome'
