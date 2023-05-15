62d3a19758a6bd90c5b7c3f9eb5c091b61492114
Konrad Wrzos <konrad@travis-ci.org>
2023-05-15 16:00:59 +0200

    Bumping clang version and fixing mongodb

--- cookbooks/travis_ci_ubuntu_2204/recipes/mongodb.rb
+++ cookbooks/travis_ci_ubuntu_2204/recipes/mongodb.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
 execute 'add_mongodb_gpg_key' do
-  command 'sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -'
+  command 'sudo wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc |  gpg --dearmor | sudo tee /usr/share/keyrings/mongodb.gpg > /dev/null'
 end
 
 execute 'add_mongodb_repository' do
-  command 'sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list'
+  command 'sudo echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list'
 end
 
-apt_update
+execute 'update_repositories' do
+  command 'sudo apt-get update -y'
+end
 
-package 'mongodb' do
+package 'mongodb-org' do
   action :install
 end
 
-service 'mongodb' do
-  action [:disable, :stop]
+service 'mongod' do
+  action %i[stop disable]
+  not_if { node['travis_build_environment']['mongodb']['service_enabled'] }
 end
