8a71d86c28c2ecf9e2e85c64878c6e9670f73830
kamildevtactics <twj_email_powizany_z_kamildevtactics>
2025-01-17 16:23:55 +0100

    Fix GPG key retrieval and add Erlang repository

--- cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
+++ cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
@@ -3,15 +3,22 @@
 remote_file '/usr/share/keyrings/erlang_solutions.asc' do
   source 'https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc'
   mode '0644'
+  action :create
 end
 
 apt_repository 'erlang_solutions' do
-  uri          'https://packages.erlang-solutions.com/ubuntu'
+  uri 'https://packages.erlang-solutions.com/ubuntu'
   distribution 'focal'
-  components   ['contrib']
-  key          '/usr/share/keyrings/erlang_solutions.asc'
+  components ['contrib']
+  key '/usr/share/keyrings/erlang_solutions.asc'
+  action :add
+end
+
+apt_update 'update_sources' do
+  action :update
 end
 
 package 'erlang' do
   action :install
 end
+
