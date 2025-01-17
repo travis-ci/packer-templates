3fc84937735236df544a757c647569d6bc51ff81
kamildevtactics <twj_email_powizany_z_kamildevtactics>
2025-01-17 14:38:24 +0100

    Final commit message summarizing all changes
    Fix GPG key retrieval and add Erlang repository

--- cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
+++ cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
@@ -1,15 +1,38 @@
+
 # frozen_string_literal: true
 
+execute 'debug_wget_erlang_key' do
+  command 'wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | tee /tmp/erlang_solutions.asc.debug'
+  not_if { ::File.exist?('/etc/apt/trusted.gpg.d/erlang_solutions.gpg') }
+end
+
 execute 'add_erlang_gpg_key' do
-  command 'wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo apt-key add -'
+  command 'wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo apt-key add - 2>&1 | tee /tmp/erlang_gpg_add.log'
+  not_if { ::File.exist?('/etc/apt/trusted.gpg.d/erlang_solutions.gpg') }
+end
+
+execute 'debug_erlang_repo' do
+  command 'echo "deb https://packages.erlang-solutions.com/ubuntu focal contrib" | tee /tmp/erlang_repo.list.debug'
+  not_if { ::File.exist?('/etc/apt/sources.list.d/rabbitmq.list') }
 end
 
 execute 'add_erlang_repository' do
-  command 'echo "deb https://packages.erlang-solutions.com/ubuntu focal contrib" | sudo tee /etc/apt/sources.list.d/rabbitmq.list'
+  command 'echo "deb https://packages.erlang-solutions.com/ubuntu focal contrib" | sudo tee /etc/apt/sources.list.d/rabbitmq.list 2>&1 | tee /tmp/erlang_repo_add.log'
+  not_if { ::File.exist?('/etc/apt/sources.list.d/rabbitmq.list') }
 end
 
-apt_update
+apt_update 'update_packages' do
+  action :update
+  ignore_failure true
+end
 
 package 'erlang' do
   action :install
+  notifies :write, 'log[erlang_installed]', :immediately
+end
+
+log 'erlang_installed' do
+  message 'Erlang package has been successfully installed.'
+  level :info
+  action :nothing
 end
