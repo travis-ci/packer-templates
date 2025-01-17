64d0398eded800a50c8cf8d7f866b0a338cc0410
kamildevtactics <twj_email_powizany_z_kamildevtactics>
2025-01-17 19:01:12 +0100

    Fix GPG key retrieval and add Erlang repository

--- cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
+++ cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
-remote_file '/usr/share/keyrings/erlang_solutions.asc' do
-  source 'https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc'
+execute 'add_erlang_gpg_key' do
+  command 'curl -fsSL https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/erlang_solutions.gpg'
+  not_if { ::File.exist?('/etc/apt/trusted.gpg.d/erlang_solutions.gpg') }
+end
+
+
+file '/etc/apt/sources.list.d/erlang_solutions.list' do
+  content 'deb [signed-by=/etc/apt/trusted.gpg.d/erlang_solutions.gpg] https://packages.erlang-solutions.com/ubuntu focal contrib'
   mode '0644'
+  owner 'root'
+  group 'root'
   action :create
 end
 
-apt_repository 'erlang_solutions' do
-  uri 'https://packages.erlang-solutions.com/ubuntu'
-  distribution 'focal'
-  components ['contrib']
-  key '/usr/share/keyrings/erlang_solutions.asc'
-  action :add
-end
 
 apt_update 'update_sources' do
   action :update
 end
+
 
 package 'erlang' do
   action :install
