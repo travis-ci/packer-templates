8cefc5906c34a0410ab1d87cadb65fc71180ad78
kamildevtactics <twj_email_powizany_z_kamildevtactics>
2025-01-20 11:38:45 +0100

    Fix GPG key retrieval and add Erlang repository

--- cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
+++ cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
@@ -1,26 +1,15 @@
 # frozen_string_literal: true
 
 execute 'add_erlang_gpg_key' do
-  command 'curl -fsSL https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/erlang_solutions.gpg'
-  not_if { ::File.exist?('/etc/apt/trusted.gpg.d/erlang_solutions.gpg') }
+  command 'wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo apt-key add -'
 end
 
-
-file '/etc/apt/sources.list.d/erlang_solutions.list' do
-  content 'deb [signed-by=/etc/apt/trusted.gpg.d/erlang_solutions.gpg] https://packages.erlang-solutions.com/ubuntu focal contrib'
-  mode '0644'
-  owner 'root'
-  group 'root'
-  action :create
+execute 'add_erlang_repository' do
+  command 'echo "deb https://packages.erlang-solutions.com/ubuntu focal contrib" | sudo tee /etc/apt/sources.list.d/rabbitmq.list'
 end
 
-
-apt_update 'update_sources' do
-  action :update
-end
-
+apt_update
 
 package 'erlang' do
   action :install
 end
-
