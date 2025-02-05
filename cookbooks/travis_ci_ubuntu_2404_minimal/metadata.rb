658d9464d8741de67d9f754efaecd9699325f281
Konrad Wrzos <konrad@travis-ci.org>
2025-02-05 15:24:44 +0100

    Syntax fix

--- cookbooks/travis_ci_ubuntu_2404_minimal/metadata.rb
+++ cookbooks/travis_ci_ubuntu_2404_minimal/metadata.rb
@@ -2,6 +2,7 @@
 
 name 'travis_ci_ubuntu_2404_minimal'
 maintainer 'Travis CI GmbH'
+maintainer_email 'contact+packer-templates@travis-ci.org'
 license 'MIT'
 description 'Installs/Configures travis_ci_ubuntu_2404_minimal'
 long_description File.read(File.join(File.dirname(__FILE__), 'README.md'))
