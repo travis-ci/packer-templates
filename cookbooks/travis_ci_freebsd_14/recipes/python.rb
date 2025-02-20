bcdfe4cf829e0066ec2e0c7f092d4e9a81331ab1
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-02-20 16:00:52 +0100

    added logs stream in chef

--- cookbooks/travis_ci_freebsd_14/recipes/python.rb
+++ cookbooks/travis_ci_freebsd_14/recipes/python.rb
@@ -4,10 +4,14 @@
 #
 #
 
-python_version = '3.9.9'
+python_version = '3.12.2'
 tarball        = "Python-#{python_version}.tgz"
 src_dir        = '/usr/local/src'
 download_url   = "https://www.python.org/ftp/python/#{python_version}/#{tarball}"
+
+log "Rozpoczynam instalację Pythona #{python_version}" do
+  level :info
+end
 
 directory src_dir do
   recursive true
@@ -29,6 +33,10 @@ bash 'build_python' do
     make -j$(sysctl -n hw.ncpu)
     make altinstall
   EOH
-  
-  not_if "/usr/local/bin/python3.9 --version | grep '#{python_version}'"
+  live_stream true
+  not_if "/usr/local/bin/python3.12 --version | grep '#{python_version}'"
+end
+
+log "Instalacja Pythona #{python_version} zakończona" do
+  level :info
 end
