bb758c374372ba36bb3c1efaeb7b78c5aa69ee2e
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-02-25 13:09:28 +0100

    erlang fix

--- cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
+++ cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-execute 'add_erlang_gpg_key' do
-  command 'wget -qO - https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo tee /usr/share/keyrings/erlang.gpg > /dev/null'
+execute 'download_erlang_gpg_key' do
+  command 'wget -qO /usr/share/keyrings/erlang.gpg https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc'
+  not_if { File.exist?('/usr/share/keyrings/erlang.gpg') }
 end
 
 execute 'add_erlang_repository' do
   command 'echo "deb [signed-by=/usr/share/keyrings/erlang.gpg] https://packages.erlang-solutions.com/ubuntu focal contrib" | sudo tee /etc/apt/sources.list.d/erlang.list'
+  not_if "grep -q 'erlang-solutions' /etc/apt/sources.list.d/erlang.list"
 end
 
 apt_update 'update_sources' do
