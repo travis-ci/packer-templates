07611581444931b8bee3d4a8fb146cd55a32463a
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-02-26 23:08:03 +0100

    fix

--- cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
+++ cookbooks/travis_ci_ubuntu_2004/recipes/erlang.rb
@@ -1,43 +1,64 @@
 # frozen_string_literal: true
 
+# Usunięcie starych plików konfiguracyjnych
 file '/etc/apt/sources.list.d/erlang.list' do
   action :delete
-  only_if { File.exist?('/etc/apt/sources.list.d/erlang.list') }
+  only_if { ::File.exist?('/etc/apt/sources.list.d/erlang.list') }
 end
 
 file '/etc/apt/sources.list.d/erlang-solutions.list' do
   action :delete
-  only_if { File.exist?('/etc/apt/sources.list.d/erlang-solutions.list') }
+  only_if { ::File.exist?('/etc/apt/sources.list.d/erlang-solutions.list') }
 end
 
 file '/usr/share/keyrings/erlang-solutions.gpg' do
   action :delete
-  only_if { File.exist?('/usr/share/keyrings/erlang-solutions.gpg') }
+  only_if { ::File.exist?('/usr/share/keyrings/erlang-solutions.gpg') }
 end
 
 file '/usr/share/keyrings/erlang.gpg' do
   action :delete
-  only_if { File.exist?('/usr/share/keyrings/erlang.gpg') }
+  only_if { ::File.exist?('/usr/share/keyrings/erlang.gpg') }
 end
 
+# Pobranie nazwy kodowej dystrybucji dynamicznie
+ruby_block 'get_distro_codename' do
+  block do
+    node.run_state['distro_codename'] = shell_out!('lsb_release -cs').stdout.strip
+  end
+  action :run
+end
+
+# Instalacja zależności
+package %w(wget gnupg) do
+  action :install
+end
+
+# Pobranie klucza GPG Erlanga
 execute 'download_erlang_gpg_key' do
   command 'wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | gpg --dearmor | tee /usr/share/keyrings/erlang-solutions.gpg > /dev/null'
-  not_if { File.exist?('/usr/share/keyrings/erlang-solutions.gpg') }
   user 'root'
+  creates '/usr/share/keyrings/erlang-solutions.gpg'
 end
 
+# Dodanie repozytorium Erlanga
 execute 'add_erlang_repository' do
-  command 'echo "deb [signed-by=/usr/share/keyrings/erlang-solutions.gpg] https://packages.erlang-solutions.com/ubuntu focal contrib" | tee /etc/apt/sources.list.d/erlang-solutions.list'
-  not_if "grep -q 'erlang-solutions' /etc/apt/sources.list.d/erlang-solutions.list"
+  command lazy {
+    "echo \"deb [signed-by=/usr/share/keyrings/erlang-solutions.gpg] https://packages.erlang-solutions.com/ubuntu #{node.run_state['distro_codename']} contrib\" | tee /etc/apt/sources.list.d/erlang-solutions.list"
+  }
   user 'root'
+  creates '/etc/apt/sources.list.d/erlang-solutions.list'
+  notifies :run, 'execute[apt_update]', :immediately
 end
 
-execute 'apt-get update' do
+# Aktualizacja apt
+execute 'apt_update' do
   command 'apt-get update -q'
   user 'root'
+  action :nothing
 end
 
-execute 'install erlang' do
-  command 'apt-get install -y erlang'
-  user 'root'
+# Instalacja Erlanga
+package 'erlang' do
+  action :install
 end
