72b9825fe2bd31a8e8948a6467df6b57df9fb102
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-04-17 16:04:40 +0200

    erlang fix

--- cookbooks/travis_ci_ubuntu_2204/recipes/erlang.rb
+++ cookbooks/travis_ci_ubuntu_2204/recipes/erlang.rb
@@ -1,17 +1,29 @@
-# frozen_string_literal: true
+# cookbooks/my_cookbook/recipes/erlang.rb
 
-execute 'add_erlang_repository' do
-  command 'echo "deb http://binaries2.erlang-solutions.com/ubuntu/ jammy-esl-erlang-25 contrib" | sudo tee /etc/apt/sources.list.d/erlang.list'
+remote_file '/usr/share/keyrings/erlang-archive-keyring.asc' do
+  source 'https://binaries2.erlang-solutions.com/GPG-KEY-pmanager.asc'
+  mode '0644'
+  action :create
+end
+
+execute 'dearmor_erlang_key' do
+  command 'gpg --dearmor < /usr/share/keyrings/erlang-archive-keyring.asc > /usr/share/keyrings/erlang-archive-keyring.gpg'
+  not_if { ::File.exist?('/usr/share/keyrings/erlang-archive-keyring.gpg') }
   action :run
 end
 
-execute 'add_erlang_gpg_key' do
-  command 'wget -O- https://binaries2.erlang-solutions.com/GPG-KEY-pmanager.asc | gpg --dearmor | sudo tee /usr/share/keyrings/erlang-archive-keyring.gpg'
-  action :run
+file '/etc/apt/sources.list.d/erlang-solutions.list' do
+  content <<~EOS
+    deb [signed-by=/usr/share/keyrings/erlang-archive-keyring.gpg] \
+    http://binaries2.erlang-solutions.com/ubuntu \
+    jammy-esl-erlang-25 contrib
+  EOS
+  mode '0644'
+  notifies :update, 'apt_update[erlang]', :immediately
 end
 
-apt_update 'update' do
-  action :update
+apt_update 'erlang' do
+  action :nothing
 end
 
 package 'erlang' do
