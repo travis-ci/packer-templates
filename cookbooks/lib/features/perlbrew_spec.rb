1a4077a527a48d8d850ae85139c2102520ce690f
Konrad Wrzos <konrad@travis-ci.org>
2025-10-14 16:37:29 +0200

    Perlbrew fix

--- cookbooks/lib/features/perlbrew_spec.rb
+++ cookbooks/lib/features/perlbrew_spec.rb
@@ -4,14 +4,14 @@ describe 'perlbrew installation' do
   describe command('perlbrew --version') do
     its(:stdout) do
       should match(
-        %r{(?:/[\w/]+)?perlbrew\s+-\s+App::perlbrew/\d+\.\d+}
+        %r{(?:/[\w/]+)?perlbrew\s*-+\s*App::perlbrew/\d+\.\d+}
       )
     end
     its(:exit_status) { should eq 0 }
   end
 
   describe command('perlbrew list') do
-    its(:stdout) { should match(/perl|5\.\d+/) } # sanity check: list has versions
+    its(:stdout) { should match(/^\s*\d+\.\d+\.\d+/) }
     its(:exit_status) { should eq 0 }
   end
 end
