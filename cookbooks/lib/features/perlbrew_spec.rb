783184edf90853b9c7d57ac396b0672cab09c011
Konrad Wrzos <konrad@travis-ci.org>
2025-10-14 16:52:08 +0200

    Perlbrew another fix

--- cookbooks/lib/features/perlbrew_spec.rb
+++ cookbooks/lib/features/perlbrew_spec.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
 describe 'perlbrew installation' do
-  describe command('perlbrew --version') do
-    its(:stdout) do
-      should match(
-        %r{(?:/[\w/]+)?perlbrew\s*-+\s*App::perlbrew/\d+\.\d+}
-      )
-    end
+  perlbrew_cmd = 'source ~/perl5/perlbrew/etc/bashrc && perlbrew'
+
+  describe command("#{perlbrew_cmd} --version") do
+    its(:stdout) { should match(/App::perlbrew\/\d+\.\d+/) }
     its(:exit_status) { should eq 0 }
   end
 
-  describe command('perlbrew list') do
+  describe command("#{perlbrew_cmd} list") do
     its(:stdout) { should match(/^\s*\d+\.\d+\.\d+/) }
     its(:exit_status) { should eq 0 }
   end
