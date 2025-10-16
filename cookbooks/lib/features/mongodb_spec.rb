609381f6f5b8fc12093eab8abeb2ae061bc20393
Konrad Wrzos <konrad@travis-ci.org>
2025-10-16 14:28:44 +0200

    Syntax again

--- cookbooks/lib/features/mongodb_spec.rb
+++ cookbooks/lib/features/mongodb_spec.rb
@@ -2,25 +2,24 @@
 
 describe 'mongodb installation' do
   describe command('mongodb --version') do
-    its(:exit_status) { should eq 0 }}
+    its(:exit_status) { should eq 0 }
   end
 
   describe 'mongodb commands' do
     before :all do
-      sh("sudo systemctl start mongod && sudo service mongod start")
+      sh('sudo systemctl start mongod && sudo service mongod start')
       procwait(/\bmongod\b/)
-      sleep 3 # HACK: thanks a bunch, Mongo
-      sh('mongosh --eval "db.testData.insert( { x : 6 } );"')
-      sleep 3 # HACK: thanks a bunch more, Mongo
+      sleep 3
+      sh('mongosh --eval "db.testData.insert({ x: 6 });"')
+      sleep 3
     end
 
-  describe command('mongosh --eval "var myCursor = db.testData.find( { x: 6 }); myCursor.forEach(printjson);"') do
-    its(:stdout) { should match(/{ "_id" : ObjectId\("\w+"\), "x" : 6 }/) }
-  end
-
+    describe command(%Q(mongosh --eval "var myCursor = db.testData.find({ x: 6 }); myCursor.forEach(printjson);")) do
+      its(:stdout) { should match(/{ "_id" : ObjectId\("\w+"\), "x" : 6 }/) }
+    end
 
     after :all do
-      sh("sudo service mongod stop || true")
+      sh('sudo systemctl stop mongod && sudo service mongod stop || true')
     end
   end
 end
