1823700dff43515cb7062123f471951bd94a80c4
Konrad Wrzos <konrad@travis-ci.org>
2025-10-16 18:25:24 +0200

    PHP

--- cookbooks/lib/features/php_interpreter_spec.rb
+++ cookbooks/lib/features/php_interpreter_spec.rb
@@ -4,13 +4,13 @@ include Support::Php
 
 if os[:arch] !~ /ppc64|aarch64|arm64/
   describe 'php interpreter' do
-    describe phpcommand('php --version') do
+    describe phpcommand('--version') do
       its(:stdout) { should be_empty }
       its(:stderr) { should match(/^PHP \d+\.\d+/) }
     end
 
     describe phpcommand(
-      %(php -r 'foreach (range(1, 5) as $i) { echo $i * 2 . " "; } echo "\n";')
+      %(-r 'foreach (range(1, 5) as $i) { echo $i * 2 . " "; } echo "\n";')
     ) do
       its(:stderr) { should be_empty }
       its(:stdout) { should match(/^2 4 6 8 10/) }
